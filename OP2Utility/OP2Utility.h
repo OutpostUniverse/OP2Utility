726294cdf1077a9797448bb18736a710638eba6e
Vagabond <Vagabond@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2017-06-18 05:38:20 +0000

    OP2Utility: Added first draft of overall project description.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/GameResources/OP2Utility@1142 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- OP2Utility/OP2Utility.h
+++ OP2Utility/OP2Utility.h
@@ -3,3 +3,12 @@
 #include "XFile.h"
 #include "MapData.h"
 #include "TileSetBmpReader.h"
+
+// OP2Utility is a utility library to aid in Outpost 2 functions typically not
+// directly related to programming new scenarios. 
+
+/*This includes
+    Loading an Outpost 2 Map file into memory.
+	Loading an Outpost 2 Tile Set BMP into memory.
+	Cross platform file system support.
+*/
