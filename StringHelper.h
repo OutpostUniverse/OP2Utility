504cafe598b4cf2507e0d8b7068c3e0f37e9fddd
Brett208 <15183337+Brett208@users.noreply.github.com>
2017-10-04 13:23:56 -1000

    Submodule fix.

--- StringHelper.h
+++ StringHelper.h
@@ -6,16 +6,8 @@
 class StringHelper
 {
 public:
-<<<<<<< HEAD
 	static void convertToUpper(std::string& str);
 	static std::string convertToUpper(const std::string& str);
 	static const char** vectorToCharArray(const std::vector<std::string>& stringVector);
 	static std::vector<std::string> removeMatchingStrings(const std::vector<std::string>& strings, const std::vector<std::string>& stringsToRemove);
 };
-=======
-	static void convertToUpper(string& str);
-	static string convertToUpper(const string& str);
-	static const char** vectorToCharArray(const vector<string>& stringVector);
-	static vector<string> removeMatchingStrings(const vector<string>& strings, const vector<string>& stringsToRemove);
-};
->>>>>>> master
