71bae2c98c0a7ed3724bb7fd5d72fd0288c3ead3
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-04-18 01:34:51 -1000

    Separate TileData struct from MapData

--- OP2Maps/MapData.h
+++ OP2Maps/MapData.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "MapHeader.h"
+#include "TileData.h"
 #include "../StreamReader.h"
 #include <vector>
 #include <string>
@@ -15,34 +16,6 @@
 //     Outpost2SVN\MapEditor\OP2Editor.
 
 #pragma pack(push, 1) // Make sure structures are byte aligned
-// Bitfield structure (32 bits total) containing metadata for a tile placed on the map.
-struct TileData
-{
-	// Determines movement speed of tile, or if tile is bulldozed, walled, tubed, or has rubble.
-	int cellType : 5;
-
-	// The tile's associated index in the TileInfo array. 
-	// TileInfo lists general properties associated with a tile.
-	unsigned int tileIndex : 11;
-
-	// Index of the unit that occupies the tile.
-	int unitIndex : 11;
-
-	// True if lava is present on tile.
-	int bLava : 1;
-
-	// True if it is possible for lava to enter the tile.
-	int bLavaPossible : 1;
-
-	// Used in controlling Lava and Microbe spread. exact workings UNKNOWN.
-	int bExpansion : 1;
-
-	// True if the Blight is present on tile.
-	int bMicrobe : 1;
-
-	// True if a wall or a building has been built on the tile.
-	int bWallOrBuilding : 1;
-};
 
 // Represents playable area of the map.
 struct ClipRect
---
+++ OP2Maps/TileData.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#pragma pack(push, 1) // Make sure structures are byte aligned
+
+// Outpost 2 Tile metadata. Implemented as a Bitfield structure (32 bits total)
+struct TileData
+{
+	// Determines movement speed of tile, or if tile is bulldozed, walled, tubed, or has rubble.
+	int cellType : 5;
+
+	// The tile's associated index in the TileInfo array. 
+	// TileInfo lists general properties associated with a tile.
+	unsigned int tileIndex : 11;
+
+	// Index of the unit that occupies the tile.
+	int unitIndex : 11;
+
+	// True if lava is present on tile.
+	int bLava : 1;
+
+	// True if it is possible for lava to enter the tile.
+	int bLavaPossible : 1;
+
+	// Used in controlling Lava and Microbe spread. exact workings UNKNOWN.
+	int bExpansion : 1;
+
+	// True if the Blight is present on tile.
+	int bMicrobe : 1;
+
+	// True if a wall or a building has been built on the tile.
+	int bWallOrBuilding : 1;
+};
+
+#pragma pack(pop)
