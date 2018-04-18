9eee5bb7bab16ddbafded5c4789fbcc17761d9cf
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-04-18 02:02:00 -1000

    Move TerrainType struct out of MapData.h file

--- OP2Maps/MapData.h
+++ OP2Maps/MapData.h
@@ -3,6 +3,7 @@
 #include "MapHeader.h"
 #include "TileData.h"
 #include "TileSetSource.h"
+#include "TerrainType.h"
 #include "../StreamReader.h"
 #include <string>
 #include <vector>
@@ -43,63 +44,6 @@ struct TileInfo
 	// The number of cycles elapsed before cycling to the next tile in an animation.
 	short animationDelay;
 };
-
-struct TileRange
-{
-	// First tile index in range.
-	short start;
-
-	// Last tile index in range.
-	short end;
-};
-
-// The properties associated with a range of tiles.
-struct TerrainType
-{
-	// Range of tiles the TerrainType applies to.
-	TileRange tileRange;
-
-	// The first tile index that represents bulldozed versions of the terrain type.
-	// The rest of the bulldozed tiles appear consecutively after the first index.
-	short bulldozedTileIndex;
-
-	// The first tile index of rubble for the Terrain Type range. 
-	// Common rubble will be the first 4 consecutive tiles after index.
-	// Rare rubble will be the next 4 consecutive tiles.
-	short rubbleTileIndex;
-
-	// Index of tube tiles. UNKOWN why the data is repeated.
-	short tubeTiles[6];
-
-	// 5 groups of 16 tiles. Each group represents a different wall type.
-	// Lava, Microbe, Full Strength Regular, Damaged Regular, and Heavily Damaged Regular.
-	short wall[5][16];
-
-	// First index for lava tiles in Terrain Type.
-	short lavaTileIndex;
-
-	// UNKOWN
-	short flat1;
-
-	// UNKNOWN
-	short flat2;
-
-	// UNKNOWN
-	short flat3;
-
-	// Tube tile indices associated with Terrain Type.
-	short tube[16];
-
-	// Index for scorched tile within Terrain Type 
-	// Scorch comes from meteor impact or vehicle destruction. Not all tiles may be scorched.
-	short scorchedTileIndex;
-
-	TileRange scorchedRange[3];
-
-	// UNKNOWN
-	short unkown[15];
-};
-
 
 
 //Represents the data stored in an Outpost 2 map file. 
