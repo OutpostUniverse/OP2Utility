00879f1b29b87d30f48ab60d0683771dcb6b086e
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-05-22 19:43:45 -1000

    Extend SeekableStreamReader to allow use in ClmFile class
     - Implement functions Length and Seek in interface SeekableStreamReader
     - Improve support for uint64_t arguments
     - Improve exception text for StreamReader constructor

--- StreamReader.cpp
+++ StreamReader.cpp
@@ -11,7 +11,7 @@ FileStreamReader::FileStreamReader(std::string filename) {
 	file = std::ifstream(filename, std::ios::in | std::ios::binary);
 
 	if (!file.is_open()) {
-		throw std::runtime_error("The following file could not be opened: " + filename + ".");
+		throw std::runtime_error("Could not open file: " + filename);
 	}
 }
 
@@ -19,7 +19,7 @@ FileStreamReader::~FileStreamReader() {
 	file.close();
 }
 
-void FileStreamReader::Read(char* buffer, size_t size) {
+void FileStreamReader::Read(char* buffer, uint64_t size) {
 	file.read(buffer, size);
 }
 
@@ -37,34 +37,58 @@ void FileStreamReader::Seek(uint64_t position) {
 	file.seekg(position, std::ios_base::beg);
 }
 
-void FileStreamReader::SeekRelative(int offset) {
+void FileStreamReader::SeekRelative(int64_t offset) {
 	file.seekg(offset, std::ios_base::cur);
 }
 
 
-MemoryStreamReader::MemoryStreamReader(char* buffer, size_t size) {
+MemoryStreamReader::MemoryStreamReader(char* buffer, uint64_t size) {
 	streamBuffer = buffer;
 	streamSize = size;
-	offset = 0;
+	position = 0;
 }
 
-void MemoryStreamReader::Read(char* buffer, size_t size) 
+void MemoryStreamReader::Read(char* buffer, uint64_t size) 
 {
-	if (offset + size > streamSize) {
+	if (position + size > streamSize) {
 		throw std::runtime_error("Size of bytes to read exceeds remaining size of buffer.");
 	}
 
-	memcpy(buffer, streamBuffer + offset, size);
-	offset += size;
+	// std::memcpy takes a _Size to copy of size_t. 
+	// Depending on the system, size_t may not be able to store the max value of uint64_t. 
+	// SIZE_MAX is the largest value that may be stored in size_t for the given system.
+	do {
+		size_t nextReadLength = SIZE_MAX;
+		// Check if remaining value of size will fit in a size_t.
+		if (size < SIZE_MAX) {
+			nextReadLength = static_cast<size_t>(size);
+		}
+
+		std::memcpy(buffer, streamBuffer + position, nextReadLength);
+		position += nextReadLength;
+		size -= nextReadLength;
+	} while (size != 0); // Continue reading in chunks until all data has been copied.
 }
 
-void MemoryStreamReader::SeekRelative(int offset)
-{
-	// Checking if offset goes below 0 is unnecessary. Arithmetic on a signed and unsigned number results 
-	// in a signed number that will wraparound to a large positive and be caught.
-	if (this->offset + offset > streamSize) {
+uint64_t MemoryStreamReader::Length() {
+	return streamSize;
+}
+
+void MemoryStreamReader::Seek(uint64_t position) {
+	if (position > this->position) {
 		throw std::runtime_error("Change in offset places read position outside bounds of buffer.");
 	}
 
-	this->offset += offset;
+	this->position = position;
+}
+
+void MemoryStreamReader::SeekRelative(int64_t offset)
+{
+	// Checking if offset goes below 0 is unnecessary. Arithmetic on a signed and unsigned number results 
+	// in a signed number that will wraparound to a large positive and be caught.
+	if (this->position + offset > streamSize) {
+		throw std::runtime_error("Change in offset places read position outside bounds of buffer.");
+	}
+
+	this->position += offset;
 }
--- StreamReader.h
+++ StreamReader.h
@@ -2,17 +2,24 @@
 
 #include <fstream>
 #include <string>
+#include <cstdint>
 
 class StreamReader {
 public:
 	virtual ~StreamReader() = 0;
-	virtual void Read(char* buffer, size_t size) = 0;
+	virtual void Read(char* buffer, uint64_t size) = 0;
 };
 
 class SeekableStreamReader : public StreamReader {
 public:
-	// Change position forward or backword in buffer.
-	virtual void SeekRelative(int offset) = 0;
+	// Get the size of the stream
+	virtual uint64_t Length() = 0;
+	
+	// Seek to absolute position, given as offset from beginning of stream
+	virtual void Seek(uint64_t position) = 0;
+	
+	// Seek by a relative amount, given as offset from current position
+	virtual void SeekRelative(int64_t offset) = 0;
 };
 
 class FileStreamReader : public SeekableStreamReader {
@@ -20,10 +27,10 @@ public:
 	FileStreamReader(std::string fileName);
 	~FileStreamReader();
 
-	void Read(char* buffer, size_t size);
+	void Read(char* buffer, uint64_t size);
 	uint64_t Length();
 	void Seek(uint64_t position);
-	void SeekRelative(int offset);
+	void SeekRelative(int64_t offset);
 
 private:
 	std::ifstream file;
@@ -32,12 +39,14 @@ private:
 
 class MemoryStreamReader : public SeekableStreamReader {
 public:
-	MemoryStreamReader(char* buffer, size_t size);
-	void Read(char* buffer, size_t size);
-	void SeekRelative(int offset);
+	MemoryStreamReader(char* buffer, uint64_t size);
+	void Read(char* buffer, uint64_t size);
+	uint64_t Length();
+	void Seek(uint64_t position);
+	void SeekRelative(int64_t offset);
 
 private:
 	char* streamBuffer;
-	size_t streamSize;
-	size_t offset;
+	uint64_t streamSize;
+	uint64_t position;
 };
