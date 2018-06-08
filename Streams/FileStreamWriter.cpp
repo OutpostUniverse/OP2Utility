c428b73103476834da395411e1bacea14cbe7c32
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-06-07 21:21:12 -1000

    Split FileStreamWriter and MemoryStreamWriter into separate files

---
+++ Streams/FileStreamWriter.cpp
@@ -0,0 +1,44 @@
+#include "FileStreamWriter.h"
+#include <stdexcept>
+
+FileStreamWriter::FileStreamWriter(const std::string& filename)
+{
+	fileStream.open(filename.c_str(), std::ios::trunc | std::ios::out | std::ios::binary);
+
+	if (!fileStream.is_open()) {
+		throw std::runtime_error("File could not be opened.");
+	}
+}
+
+FileStreamWriter::~FileStreamWriter() {
+	fileStream.close();
+}
+
+void FileStreamWriter::Write(const void* buffer, size_t size)
+{
+	fileStream.write(static_cast<const char*>(buffer), size);
+}
+
+uint64_t FileStreamWriter::Length()
+{
+	auto currentPosition = fileStream.tellp();  // Record current position
+	fileStream.seekp(0, std::ios_base::end);    // Seek to end of file
+	auto length = fileStream.tellp();   // Record current position (length of file)
+	fileStream.seekp(currentPosition);  // Restore position
+	return length;
+}
+
+uint64_t FileStreamWriter::Position()
+{
+	return fileStream.tellp();  // Return the current put pointer
+}
+
+void FileStreamWriter::Seek(uint64_t offset)
+{
+	fileStream.seekp(offset);
+}
+
+void FileStreamWriter::SeekRelative(int64_t offset)
+{
+	fileStream.seekp(offset, std::ios_base::cur);
+}
--- Streams/StreamWriter.h
+++ Streams/StreamWriter.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <fstream>
-#include <string>
+#include <cstdint>
 
 class StreamWriter
 {
@@ -26,58 +25,5 @@ public:
 	virtual void SeekRelative(int64_t offset) = 0;
 };
 
-class FileStreamWriter : public SeekableStreamWriter
-{
-public:
-	FileStreamWriter(const std::string& filename);
-
-	// StreamWriter methods
-	~FileStreamWriter() override;
-	void Write(const void* buffer, size_t size) override;
-
-	// Inline templated convenience methods, to easily read arbitrary data types
-	template<typename T> inline void Write(const T& object) {
-		Write((char*)&object, sizeof(object));
-	}
-
-	// SeekableStreamWriter methods
-	uint64_t Length() override;
-	uint64_t Position() override;
-	void Seek(uint64_t offset) override;
-	void SeekRelative(int64_t offset) override;
-
-private:
-	std::fstream fileStream;
-};
-
-class MemoryStreamWriter : public SeekableStreamWriter
-{
-public:
-	// buffer: where data will be written to.
-	// size: Amount of space allocated in the buffer for writing into.
-	MemoryStreamWriter(void* buffer, size_t size);
-
-	// StreamWriter methods
-	void Write(const void* buffer, size_t size) override;
-
-	// Inline templated convenience methods, to easily read arbitrary data types
-	template<typename T> inline void Write(const T& object) {
-		Write((char*)&object, sizeof(object));
-	}
-
-	// SeekableStreamWriter methods
-	uint64_t Length() override;
-	uint64_t Position() override;
-	void Seek(uint64_t offset) override;
-	void SeekRelative(int64_t offset) override;
-
-private:
-	// Memory location to write data into.
-	char* streamBuffer;
-
-	// Size of the provided streamBuffer.
-	size_t streamSize;
-
-	// Current location in the streamBuffer.
-	size_t offset;
-};
+#include "FileStreamWriter.h"
+#include "MemoryStreamWriter.h"
