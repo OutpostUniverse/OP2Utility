3883d71110f9ae50e9bae7ad1276bf361c886e5a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-06-10 21:34:56 +0700

    Convert MapReader/Writer to namespace
    Private methods are wrapped in an anonymous namespace to prevent the
    possibility of name collision.
    
    StreamReader/Writer parameter added to all private methods.

--- Maps/MapReader.h
+++ Maps/MapReader.h
@@ -7,24 +7,7 @@
 
 class SeekableStreamReader;
 
-class MapReader
-{
-public:
+namespace MapReader {
 	MapData Read(std::string filename, bool savedGame = false);
 	MapData Read(SeekableStreamReader& mapStream, bool savedGame = false);
-
-private:
-	SeekableStreamReader& streamReader;
-
-	void SkipSaveGameHeader();
-	void ReadHeader(MapData& mapData);
-	void ReadTiles(MapData& mapData);
-	void ReadClipRect(ClipRect& clipRect);
-	void ReadTilesetSources(MapData& mapData);
-	void ReadTilesetHeader();
-	void ReadTileInfo(MapData& mapData);
-	void ReadVersionTag();
-	void ReadTileGroups(MapData& mapData);
-	TileGroup ReadTileGroup();
-	std::string ReadString();
-};
+}
--- Maps/MapWriter.h
+++ Maps/MapWriter.h
@@ -8,24 +8,7 @@
 class SeekableStreamWriter;
 
 // Writes an Outpost 2 map to file.
-class MapWriter
-{
-public:
+namespace MapWriter {
 	void Write(const std::string& filename, const MapData& mapData);
 	void Write(SeekableStreamWriter& mapStream, const MapData& mapData);
-
-private:
-	SeekableStreamWriter& streamWriter;
-
-	void WriteHeader(const MapHeader& header);
-	void WriteTiles(const std::vector<TileData>& tiles);
-	void WriteClipRect(const ClipRect& clipRect);
-	void WriteTilesetSources(const std::vector<TilesetSource>& tilesetSources);
-	void WriteTilesetHeader();
-	void WriteTileInfo(const std::vector<TileInfo>& tileInfos);
-	void WriteTerrainType(const std::vector<TerrainType>& terrainTypes);
-	void WriteTileGroups(const std::vector<TileGroup>& tileGroups);
-	void WriteVersionTag(int versionTag);
-	void WriteContainerSize(size_t size);
-	void WriteString(const std::string& s);
-};
+}
