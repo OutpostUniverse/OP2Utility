ce5758a8b0c25698f2a2480f8e2c65aee915dca0
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-06-10 21:34:56 +0700

    Convert MapReader/Writer to namespace
    Private methods are wrapped in an anonymous namespace to prevent the
    possibility of name collision.
    
    StreamReader/Writer parameter added to all private methods.

--- Maps/MapReader.cpp
+++ Maps/MapReader.cpp
@@ -4,149 +4,175 @@
 #include <stdexcept>
 #include <cstring>
 
-MapData MapReader::Read(std::string filename, bool savedGame)
-{
-	FileStreamReader mapReader(filename);
-	return Read(mapReader, savedGame);
-}
 
-MapData MapReader::Read(SeekableStreamReader& mapStream, bool savedGame)
-{
-	streamReader = mapStream;
-
-	MapData mapData;
-
-	if (savedGame) {
-		SkipSaveGameHeader();
+namespace MapReader {
+	// Anonymous namespace to hold private methods
+	namespace {
+		void SkipSaveGameHeader(SeekableStreamReader& streamReader);
+		void ReadHeader(StreamReader& streamReader, MapData& mapData);
+		void ReadTiles(StreamReader& streamReader, MapData& mapData);
+		void ReadClipRect(StreamReader& streamReader, ClipRect& clipRect);
+		void ReadTilesetSources(StreamReader& streamReader, MapData& mapData);
+		void ReadTilesetHeader(StreamReader& streamReader);
+		void ReadTileInfo(StreamReader& streamReader, MapData& mapData);
+		void ReadVersionTag(StreamReader& streamReader);
+		void ReadTileGroups(SeekableStreamReader& streamReader, MapData& mapData);
+		TileGroup ReadTileGroup(StreamReader& streamReader);
+		std::string ReadString(StreamReader& streamReader);
 	}
 
-	ReadHeader(mapData);
-	ReadTiles(mapData);
-	ReadClipRect(mapData.clipRect);
-	ReadTilesetSources(mapData);
-	ReadTilesetHeader();
-	ReadTileInfo(mapData);
-	ReadVersionTag();
-	ReadVersionTag();
-	ReadTileGroups(mapData);
 
-	return mapData;
-}
+	// ==== Public methohds ====
 
-void MapReader::SkipSaveGameHeader()
-{
-	streamReader.SeekRelative(0x1E025);
-}
 
-void MapReader::ReadHeader(MapData& mapData)
-{
-	streamReader.Read(&mapData.header, sizeof(mapData.header));
-}
-
-void MapReader::ReadTiles(MapData& mapData)
-{
-	mapData.tiles.resize(mapData.header.TileCount());
-	streamReader.Read(&mapData.tiles[0], mapData.tiles.size() * sizeof(TileData));
-}
-
-void MapReader::ReadClipRect(ClipRect& clipRect)
-{
-	streamReader.Read(&clipRect, sizeof(clipRect));
-}
-
-void MapReader::ReadTilesetHeader()
-{
-	char buffer[10];
-	streamReader.Read(buffer, sizeof(buffer));
-
-	if (std::strncmp(buffer, "TILE SET\x1a", sizeof(buffer))) {
-		throw std::runtime_error("'TILE SET' string not found.");
-	}
-}
-
-void MapReader::ReadTilesetSources(MapData& mapData)
-{
-	mapData.tilesetSources.resize(static_cast<size_t>(mapData.header.numTilesets));
-
-	for (unsigned int i = 0; i < mapData.header.numTilesets; ++i)
+	MapData Read(std::string filename, bool savedGame)
 	{
-		mapData.tilesetSources[i].tilesetFilename = ReadString();
+		FileStreamReader mapReader(filename);
+		return Read(mapReader, savedGame);
+	}
 
-		if (mapData.tilesetSources[i].tilesetFilename.size() > 8) {
-			throw std::runtime_error("Tileset name may not be greater than 8 characters in length.");
+	MapData Read(SeekableStreamReader& streamReader, bool savedGame)
+	{
+		MapData mapData;
+
+		if (savedGame) {
+			SkipSaveGameHeader(streamReader);
 		}
 
-		if (mapData.tilesetSources[i].tilesetFilename.size() > 0) {
-			streamReader.Read(&mapData.tilesetSources[i].numTiles, sizeof(int));
+		ReadHeader(streamReader, mapData);
+		ReadTiles(streamReader, mapData);
+		ReadClipRect(streamReader, mapData.clipRect);
+		ReadTilesetSources(streamReader, mapData);
+		ReadTilesetHeader(streamReader);
+		ReadTileInfo(streamReader, mapData);
+		ReadVersionTag(streamReader);
+		ReadVersionTag(streamReader);
+		ReadTileGroups(streamReader, mapData);
+
+		return mapData;
+	}
+
+
+	// == Private methods ==
+
+
+	namespace {
+		void SkipSaveGameHeader(SeekableStreamReader& streamReader)
+		{
+			streamReader.SeekRelative(0x1E025);
+		}
+
+		void ReadHeader(StreamReader& streamReader, MapData& mapData)
+		{
+			streamReader.Read(&mapData.header, sizeof(mapData.header));
+		}
+
+		void ReadTiles(StreamReader& streamReader, MapData& mapData)
+		{
+			mapData.tiles.resize(mapData.header.TileCount());
+			streamReader.Read(&mapData.tiles[0], mapData.tiles.size() * sizeof(TileData));
+		}
+
+		void ReadClipRect(StreamReader& streamReader, ClipRect& clipRect)
+		{
+			streamReader.Read(&clipRect, sizeof(clipRect));
+		}
+
+		void ReadTilesetHeader(StreamReader& streamReader)
+		{
+			char buffer[10];
+			streamReader.Read(buffer, sizeof(buffer));
+
+			if (std::strncmp(buffer, "TILE SET\x1a", sizeof(buffer))) {
+				throw std::runtime_error("'TILE SET' string not found.");
+			}
+		}
+
+		void ReadTilesetSources(StreamReader& streamReader, MapData& mapData)
+		{
+			mapData.tilesetSources.resize(static_cast<size_t>(mapData.header.numTilesets));
+
+			for (unsigned int i = 0; i < mapData.header.numTilesets; ++i)
+			{
+				mapData.tilesetSources[i].tilesetFilename = ReadString(streamReader);
+
+				if (mapData.tilesetSources[i].tilesetFilename.size() > 8) {
+					throw std::runtime_error("Tileset name may not be greater than 8 characters in length.");
+				}
+
+				if (mapData.tilesetSources[i].tilesetFilename.size() > 0) {
+					streamReader.Read(&mapData.tilesetSources[i].numTiles, sizeof(int));
+				}
+			}
+		}
+
+		void ReadTileInfo(StreamReader& streamReader, MapData& mapData)
+		{
+			size_t numTileInfo;
+			streamReader.Read(&numTileInfo, sizeof(numTileInfo));
+
+			mapData.tileInfos.resize(numTileInfo);
+			streamReader.Read(&mapData.tileInfos[0], numTileInfo * sizeof(TileInfo));
+
+			size_t numTerrainTypes;
+			streamReader.Read(&numTerrainTypes, sizeof(numTerrainTypes));
+			mapData.terrainTypes.resize(numTerrainTypes);
+			streamReader.Read(&mapData.terrainTypes[0], numTerrainTypes * sizeof(TerrainType));
+		}
+
+		void ReadVersionTag(StreamReader& streamReader)
+		{
+			int versionTag;
+			streamReader.Read(&versionTag, sizeof(versionTag));
+
+			if (versionTag < 0x1010)
+			{
+				std::cerr << "All instances of version tag in .map and .op2 files should be greater than 0x1010.";
+			}
+		}
+
+		void ReadTileGroups(SeekableStreamReader& streamReader, MapData& mapData)
+		{
+			int numTileGroups;
+			streamReader.Read(&numTileGroups, sizeof(numTileGroups));
+			streamReader.SeekRelative(4);
+
+			for (int i = 0; i < numTileGroups; ++i)
+			{
+				mapData.tileGroups.push_back(ReadTileGroup(streamReader));
+			}
+		}
+
+		TileGroup ReadTileGroup(StreamReader& streamReader)
+		{
+			TileGroup tileGroup;
+
+			streamReader.Read(&tileGroup.tileWidth, sizeof(tileGroup.tileWidth));
+			streamReader.Read(&tileGroup.tileHeight, sizeof(tileGroup.tileHeight));
+
+			int mappingIndex;
+			for (int i = 0; i < tileGroup.tileHeight * tileGroup.tileWidth; ++i)
+			{
+				streamReader.Read(&mappingIndex, sizeof(mappingIndex));
+				tileGroup.mappingIndices.push_back(mappingIndex);
+			}
+
+			tileGroup.name = ReadString(streamReader);
+
+			return tileGroup;
+		}
+
+		// String must be stored in file as string length followed by char[].
+		std::string ReadString(StreamReader& streamReader)
+		{
+			uint32_t stringLength;
+			streamReader.Read(&stringLength, sizeof(stringLength));
+
+			std::string s;
+			s.resize(stringLength);
+			streamReader.Read(&s[0], stringLength);
+
+			return s;
 		}
 	}
-}
-
-void MapReader::ReadTileInfo(MapData& mapData)
-{
-	size_t numTileInfo;
-	streamReader.Read(&numTileInfo, sizeof(numTileInfo));
-
-	mapData.tileInfos.resize(numTileInfo);
-	streamReader.Read(&mapData.tileInfos[0], numTileInfo * sizeof(TileInfo));
-
-	size_t numTerrainTypes;
-	streamReader.Read(&numTerrainTypes, sizeof(numTerrainTypes));
-	mapData.terrainTypes.resize(numTerrainTypes);
-	streamReader.Read(&mapData.terrainTypes[0], numTerrainTypes * sizeof(TerrainType));
-}
-
-void MapReader::ReadVersionTag()
-{
-	int versionTag;
-	streamReader.Read(&versionTag, sizeof(versionTag));
-
-	if (versionTag < 0x1010)
-	{
-		std::cerr << "All instances of version tag in .map and .op2 files should be greater than 0x1010.";
-	}
-}
-
-void MapReader::ReadTileGroups(MapData& mapData)
-{
-	int numTileGroups;
-	streamReader.Read(&numTileGroups, sizeof(numTileGroups));
-	streamReader.SeekRelative(4);
-
-	for (int i = 0; i < numTileGroups; ++i)
-	{
-		mapData.tileGroups.push_back(ReadTileGroup());
-	}
-}
-
-TileGroup MapReader::ReadTileGroup()
-{
-	TileGroup tileGroup;
-
-	streamReader.Read(&tileGroup.tileWidth, sizeof(tileGroup.tileWidth));
-	streamReader.Read(&tileGroup.tileHeight, sizeof(tileGroup.tileHeight));
-
-	int mappingIndex;
-	for (int i = 0; i < tileGroup.tileHeight * tileGroup.tileWidth; ++i)
-	{
-		streamReader.Read(&mappingIndex, sizeof(mappingIndex));
-		tileGroup.mappingIndices.push_back(mappingIndex);
-	}
-
-	tileGroup.name = ReadString();
-
-	return tileGroup;
-}
-
-// String must be stored in file as string length followed by char[].
-std::string MapReader::ReadString()
-{
-	uint32_t stringLength;
-	streamReader.Read(&stringLength, sizeof(stringLength));
-
-	std::string s;
-	s.resize(stringLength);
-	streamReader.Read(&s[0], stringLength);
-
-	return s;
 }
--- Maps/MapWriter.cpp
+++ Maps/MapWriter.cpp
@@ -1,125 +1,151 @@
 #include "MapWriter.h"
 #include "../Streams/FileStreamWriter.h"
 
-void MapWriter::Write(const std::string& filename, const MapData& mapData)
-{
-	FileStreamWriter mapWriter(filename);
-	Write(mapWriter, mapData);
-}
 
-void MapWriter::Write(SeekableStreamWriter& mapStream, const MapData& mapData)
-{	
-	streamWriter = mapStream;
-
-	WriteHeader(mapData.header);
-	WriteTiles(mapData.tiles);
-	WriteClipRect(mapData.clipRect);
-	WriteTilesetSources(mapData.tilesetSources);
-	WriteTilesetHeader();
-	WriteTileInfo(mapData.tileInfos);
-	WriteTerrainType(mapData.terrainTypes);
-
-	WriteVersionTag(mapData.header.versionTag);
-	WriteVersionTag(mapData.header.versionTag);
-
-	WriteTileGroups(mapData.tileGroups);
-}
-
-void MapWriter::WriteHeader(const MapHeader& header)
-{
-	if (!header.VersionTagValid())
-	{
-		throw std::runtime_error("All instances of version tag in .map and .op2 files must be greater than 0x1010.");
+namespace MapWriter {
+	// Anonymous namespace to hold private methods
+	namespace {
+		void WriteHeader(StreamWriter& streamWriter, const MapHeader& header);
+		void WriteTiles(StreamWriter& streamWriter, const std::vector<TileData>& tiles);
+		void WriteClipRect(StreamWriter& streamWriter, const ClipRect& clipRect);
+		void WriteTilesetSources(StreamWriter& streamWriter, const std::vector<TilesetSource>& tilesetSources);
+		void WriteTilesetHeader(StreamWriter& streamWriter);
+		void WriteTileInfo(StreamWriter& streamWriter, const std::vector<TileInfo>& tileInfos);
+		void WriteTerrainType(StreamWriter& streamWriter, const std::vector<TerrainType>& terrainTypes);
+		void WriteTileGroups(SeekableStreamWriter& streamWriter, const std::vector<TileGroup>& tileGroups);
+		void WriteVersionTag(StreamWriter& streamWriter, int versionTag);
+		void WriteContainerSize(StreamWriter& streamWriter, size_t size);
+		void WriteString(StreamWriter& streamWriter, const std::string& s);
 	}
 
-	streamWriter.Write(&header, sizeof(header));
-}
 
-void MapWriter::WriteVersionTag(int versionTag)
-{
-	streamWriter.Write(&versionTag, sizeof(versionTag));
-}
+	// ==== Public methods ====
 
-void MapWriter::WriteTiles(const std::vector<TileData>& tiles)
-{
-	streamWriter.Write(&tiles[0], tiles.size() * sizeof(TileData));
-}
 
-void MapWriter::WriteClipRect(const ClipRect& clipRect)
-{
-	streamWriter.Write(&clipRect, sizeof(clipRect));
-}
-
-void MapWriter::WriteTilesetSources(const std::vector<TilesetSource>& tileSetSources)
-{
-	for (TilesetSource tilesetSource : tileSetSources)
+	void Write(const std::string& filename, const MapData& mapData)
 	{
-		WriteString(tilesetSource.tilesetFilename);
+		FileStreamWriter mapWriter(filename);
+		Write(mapWriter, mapData);
+	}
 
-		// Only include the number of tiles if the tileset contains a filename.
-		if (tilesetSource.tilesetFilename.size() > 0)
+	void Write(SeekableStreamWriter& streamWriter, const MapData& mapData)
+	{
+		WriteHeader(streamWriter, mapData.header);
+		WriteTiles(streamWriter, mapData.tiles);
+		WriteClipRect(streamWriter, mapData.clipRect);
+		WriteTilesetSources(streamWriter, mapData.tilesetSources);
+		WriteTilesetHeader(streamWriter);
+		WriteTileInfo(streamWriter, mapData.tileInfos);
+		WriteTerrainType(streamWriter, mapData.terrainTypes);
+
+		WriteVersionTag(streamWriter, mapData.header.versionTag);
+		WriteVersionTag(streamWriter, mapData.header.versionTag);
+
+		WriteTileGroups(streamWriter, mapData.tileGroups);
+	}
+
+
+	// == Private methods ==
+
+
+	namespace {
+		void WriteHeader(StreamWriter& streamWriter, const MapHeader& header)
 		{
-			streamWriter.Write(&tilesetSource.numTiles, sizeof(int));
-		}
-	}
-}
+			if (!header.VersionTagValid())
+			{
+				throw std::runtime_error("All instances of version tag in .map and .op2 files must be greater than 0x1010.");
+			}
 
-void MapWriter::WriteTilesetHeader()
-{
-	streamWriter.Write("TILE SET\x1a", 10);
-}
-
-void MapWriter::WriteTileInfo(const std::vector<TileInfo>& tileInfos)
-{
-	WriteContainerSize(tileInfos.size());
-
-	for (TileInfo tileInfo : tileInfos)
-	{
-		streamWriter.Write(&tileInfo, sizeof(TileInfo));
-	}
-}
-
-void MapWriter::WriteTerrainType(const std::vector<TerrainType>& terrainTypes)
-{
-	WriteContainerSize(terrainTypes.size());
-
-	for (TerrainType terrainType : terrainTypes)
-	{
-		streamWriter.Write(&terrainType, sizeof(TerrainType));
-	}
-}
-
-void MapWriter::WriteTileGroups(const std::vector<TileGroup>& tileGroups)
-{
-	WriteContainerSize(tileGroups.size());
-
-	streamWriter.SeekRelative(sizeof(int));
-
-	for (TileGroup tileGroup : tileGroups)
-	{
-		streamWriter.Write(&tileGroup.tileWidth, sizeof(tileGroup.tileWidth));
-		streamWriter.Write(&tileGroup.tileHeight, sizeof(tileGroup.tileHeight));
-
-		for (int mappingIndex : tileGroup.mappingIndices)
-		{
-			streamWriter.Write(&mappingIndex, sizeof(mappingIndex));
+			streamWriter.Write(&header, sizeof(header));
 		}
 
-		WriteString(tileGroup.name);
-	}
-}
+		void WriteVersionTag(StreamWriter& streamWriter, int versionTag)
+		{
+			streamWriter.Write(&versionTag, sizeof(versionTag));
+		}
 
-void MapWriter::WriteContainerSize(size_t size)
-{
-	streamWriter.Write(&size, sizeof(size));
-}
+		void WriteTiles(StreamWriter& streamWriter, const std::vector<TileData>& tiles)
+		{
+			streamWriter.Write(&tiles[0], tiles.size() * sizeof(TileData));
+		}
 
-// String must be stored in file as string length followed by char[].
-void MapWriter::WriteString(const std::string& s)
-{
-	WriteContainerSize(s.size());
+		void WriteClipRect(StreamWriter& streamWriter, const ClipRect& clipRect)
+		{
+			streamWriter.Write(&clipRect, sizeof(clipRect));
+		}
 
-	if (s.size() > 0) {
-		streamWriter.Write(s.c_str(), s.size());
+		void WriteTilesetSources(StreamWriter& streamWriter, const std::vector<TilesetSource>& tileSetSources)
+		{
+			for (TilesetSource tilesetSource : tileSetSources)
+			{
+				WriteString(streamWriter, tilesetSource.tilesetFilename);
+
+				// Only include the number of tiles if the tileset contains a filename.
+				if (tilesetSource.tilesetFilename.size() > 0)
+				{
+					streamWriter.Write(&tilesetSource.numTiles, sizeof(int));
+				}
+			}
+		}
+
+		void WriteTilesetHeader(StreamWriter& streamWriter)
+		{
+			streamWriter.Write("TILE SET\x1a", 10);
+		}
+
+		void WriteTileInfo(StreamWriter& streamWriter, const std::vector<TileInfo>& tileInfos)
+		{
+			WriteContainerSize(streamWriter, tileInfos.size());
+
+			for (TileInfo tileInfo : tileInfos)
+			{
+				streamWriter.Write(&tileInfo, sizeof(TileInfo));
+			}
+		}
+
+		void WriteTerrainType(StreamWriter& streamWriter, const std::vector<TerrainType>& terrainTypes)
+		{
+			WriteContainerSize(streamWriter, terrainTypes.size());
+
+			for (TerrainType terrainType : terrainTypes)
+			{
+				streamWriter.Write(&terrainType, sizeof(TerrainType));
+			}
+		}
+
+		void WriteTileGroups(SeekableStreamWriter& streamWriter, const std::vector<TileGroup>& tileGroups)
+		{
+			WriteContainerSize(streamWriter, tileGroups.size());
+
+			streamWriter.SeekRelative(sizeof(int));
+
+			for (TileGroup tileGroup : tileGroups)
+			{
+				streamWriter.Write(&tileGroup.tileWidth, sizeof(tileGroup.tileWidth));
+				streamWriter.Write(&tileGroup.tileHeight, sizeof(tileGroup.tileHeight));
+
+				for (int mappingIndex : tileGroup.mappingIndices)
+				{
+					streamWriter.Write(&mappingIndex, sizeof(mappingIndex));
+				}
+
+				WriteString(streamWriter, tileGroup.name);
+			}
+		}
+
+		void WriteContainerSize(StreamWriter& streamWriter, size_t size)
+		{
+			streamWriter.Write(&size, sizeof(size));
+		}
+
+		// String must be stored in file as string length followed by char[].
+		void WriteString(StreamWriter& streamWriter, const std::string& s)
+		{
+			WriteContainerSize(streamWriter, s.size());
+
+			if (s.size() > 0) {
+				streamWriter.Write(s.c_str(), s.size());
+			}
+		}
 	}
 }
