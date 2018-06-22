42f673f3b99d5de8f17901cd4b43007d6b950d7d
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-06-21 19:27:28 -1000

    Create a namespace alias 'fs' to represent experimental filesystem in XFile.cpp

--- XFile.cpp
+++ XFile.cpp
@@ -5,11 +5,11 @@
 #include <filesystem>
 #include <experimental/filesystem>
 
-using namespace std::experimental::filesystem;
+namespace fs = std::experimental::filesystem;
 
 std::string XFile::GetFileExtension(const std::string& pathStr)
 {
-	return path(pathStr).extension().string();
+	return fs::path(pathStr).extension().string();
 }
 
 bool XFile::IsDirectory(const std::string& pathStr)
@@ -18,12 +18,12 @@ bool XFile::IsDirectory(const std::string& pathStr)
 		return true;
 	}
 
-	return is_directory(pathStr);
+	return fs::is_directory(pathStr);
 }
 
 bool XFile::IsFile(const std::string& path)
 {
-	return is_regular_file(path);
+	return fs::is_regular_file(path);
 }
 
 bool XFile::ExtensionMatches(const std::string& pathStr, const std::string& extension)
@@ -42,24 +42,24 @@ bool XFile::ExtensionMatches(const std::string& pathStr, const std::string& exte
 
 std::string XFile::ChangeFileExtension(const std::string& filename, const std::string& newExtension)
 {
-	return path(filename).replace_extension(newExtension).string();
+	return fs::path(filename).replace_extension(newExtension).string();
 }
 
 void XFile::NewDirectory(const std::string& newPath)
 {
-	create_directory(path(newPath));
+	fs::create_directory(fs::path(newPath));
 }
 
 bool XFile::PathExists(const std::string& pathStr)
 {
-	return exists(path(pathStr));
+	return fs::exists(fs::path(pathStr));
 }
 
 std::string XFile::AppendToFilename(const std::string& filename, const std::string& valueToAppend)
 {
-	path newPath(filename);
+	fs::path newPath(filename);
 	
-	path newFilename = newPath.filename().replace_extension("");
+	fs::path newFilename = newPath.filename().replace_extension("");
 	newFilename += valueToAppend + newPath.extension().string();
 
 	newPath.replace_filename(newFilename);
@@ -73,7 +73,7 @@ std::vector<std::string> XFile::GetFilesFromDirectory(const std::string& directo
 	auto pathStr = directory.length() > 0 ? directory : "./";
 
 	std::vector<std::string> filenames;
-	for (auto& entry : directory_iterator(pathStr)) {
+	for (auto& entry : fs::directory_iterator(pathStr)) {
 		filenames.push_back(entry.path().string());
 	}
 
@@ -108,7 +108,7 @@ std::vector<std::string> XFile::GetFilesFromDirectory(const std::string& directo
 			return filenames;
 		}
 
-		std::string extension = path(filenames[i]).extension().string();
+		std::string extension = fs::path(filenames[i]).extension().string();
 		if (extension != fileType) {
 			filenames.erase(filenames.begin() + i);
 		}
@@ -123,13 +123,13 @@ std::vector<std::string> XFile::GetFilesFromDirectory(const std::string& directo
 
 bool XFile::IsRootPath(const std::string& pathStr)
 {
-	return path(pathStr).has_root_path();
+	return fs::path(pathStr).has_root_path();
 }
 
 std::string XFile::ReplaceFilename(const std::string& pathStr, const std::string& filenameStr)
 {
-	path p(pathStr);
-	path filename = path(filenameStr).filename();
+	fs::path p(pathStr);
+	fs::path filename = fs::path(filenameStr).filename();
 	
 	// Brett208 22JUL17: There seems to be a bug in path.replace_filename that removes a directory if it has a space in it on MSVC.
 
@@ -138,11 +138,11 @@ std::string XFile::ReplaceFilename(const std::string& pathStr, const std::string
 
 std::string XFile::AppendSubDirectory(const std::string& pathStr, const std::string& subDirectory)
 {
-	path p(pathStr);
-	path filename(p.filename());
+	fs::path p(pathStr);
+	fs::path filename(p.filename());
 
 	if (p == filename) {
-		return path().append(subDirectory).append(filename).string();
+		return fs::path().append(subDirectory).append(filename).string();
 	}
 
 	p = p.remove_filename();
@@ -151,12 +151,12 @@ std::string XFile::AppendSubDirectory(const std::string& pathStr, const std::str
 
 std::string XFile::GetFilename(const std::string& pathStr)
 {
-	return path(pathStr).filename().string();
+	return fs::path(pathStr).filename().string();
 }
 
 std::string XFile::RemoveFilename(const std::string& pathStr)
 {
-	return path(pathStr).remove_filename().string();
+	return fs::path(pathStr).remove_filename().string();
 }
 
 bool XFile::PathsAreEqual(std::string pathStr1, std::string pathStr2)
@@ -164,12 +164,12 @@ bool XFile::PathsAreEqual(std::string pathStr1, std::string pathStr2)
 	StringHelper::ConvertToUpper(pathStr1);
 	StringHelper::ConvertToUpper(pathStr2);
 
-	path path1(pathStr1);
+	fs::path path1(pathStr1);
 	if (path1.has_relative_path() && path1.relative_path() == path1.filename()) {
 		path1 = ("./" + pathStr1);
 	}
 
-	path path2(pathStr2);
+	fs::path path2(pathStr2);
 	if (path2.has_relative_path() && path2.relative_path() == path2.filename()) {
 		path2 = ("./" + pathStr2);
 	}
@@ -179,7 +179,7 @@ bool XFile::PathsAreEqual(std::string pathStr1, std::string pathStr2)
 
 std::string XFile::GetDirectory(const std::string& pathStr)
 {
-	path p(pathStr);
+	fs::path p(pathStr);
 
 	if (p.has_relative_path()) 
 	{
@@ -199,10 +199,10 @@ std::string XFile::GetDirectory(const std::string& pathStr)
 
 void XFile::DeletePath(const std::string& pathStr)
 {
-	remove_all(pathStr);
+	fs::remove_all(pathStr);
 }
 
 void XFile::RenameFile(const std::string& oldPath, const std::string& newPath) 
 {
-	rename(oldPath, newPath);
+	fs::rename(oldPath, newPath);
 }
