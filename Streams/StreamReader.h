291e5c72f16c9deb0b54f4d25a30d516e9995a18
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-06-21 18:59:32 -1000

    Improve Read/Writer implementation function comment

--- Streams/StreamReader.h
+++ Streams/StreamReader.h
@@ -16,7 +16,7 @@ public:
 	}
 
 protected:
-	// When creating child classes, override this function to implement new read functionality
-	// This function is required because to allow the base class to contain the Read template function.
+	// ReadImplementation is named differently from Read to prevent name hiding of the 
+	// Read template helpers in derived classes.
 	virtual void ReadImplementation(void* buffer, std::size_t size) = 0;
 };
