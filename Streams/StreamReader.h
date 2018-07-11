4f0a0bc632ed9c1d5822e36ecf31f0f8b0ade4d5
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-07-11 14:20:54 +0700

    Reorganize StreamReader/StreamWriter interfaces
    This places helper methods at the bottom, which depend only on the core
    interface above.
    
    Side note: As a micro optimization, putting the most common virtual
    method first (ReadImplementation/WriteImplementation) means the
    method will appear first in the virtual function table. This means the
    method is access with an offset of 0, and instructions with no offset
    have a smaller encoding. Hence, calls to this method will have a
    slightly more compact instruction encoding.
    
    Organizational note: This also places the core method for this interface
    first, giving it the prominence it deserves.

--- Streams/StreamReader.h
+++ Streams/StreamReader.h
@@ -3,8 +3,16 @@
 #include <cstddef>
 
 class StreamReader {
+protected:
+	// ReadImplementation is named differently from Read to prevent name hiding of the
+	// Read template helpers in derived classes.
+	virtual void ReadImplementation(void* buffer, std::size_t size) = 0;
+
 public:
 	virtual ~StreamReader() = default;
+
+	// Helper methods, which depend only on the above interface
+	// ====
 
 	inline void Read(void* buffer, std::size_t size) {
 		ReadImplementation(buffer, size);
@@ -14,9 +22,4 @@ public:
 	template<typename T> inline void Read(T& object) {
 		ReadImplementation(&object, sizeof(object));
 	}
-
-protected:
-	// ReadImplementation is named differently from Read to prevent name hiding of the 
-	// Read template helpers in derived classes.
-	virtual void ReadImplementation(void* buffer, std::size_t size) = 0;
 };
--- Streams/StreamWriter.h
+++ Streams/StreamWriter.h
@@ -3,8 +3,16 @@
 #include <cstddef>
 
 class StreamWriter {
+protected:
+	// WriteImplementation is named differently from Write to prevent name hiding of the
+	// Write template helpers in derived classes.
+	virtual void WriteImplementation(const void* buffer, std::size_t size) = 0;
+
 public:
 	virtual ~StreamWriter() = default;
+
+	// Helper methods, which depend only on the above interface
+	// ====
 
 	inline void Write(const void* buffer, std::size_t size) {
 		WriteImplementation(buffer, size);
@@ -14,9 +22,4 @@ public:
 	template<typename T> inline void Write(const T& object) {
 		WriteImplementation(&object, sizeof(object));
 	}
-
-protected:
-	// WriteImplementation is named differently from Write to prevent name hiding of the 
-	// Write template helpers in derived classes.
-	virtual void WriteImplementation(const void* buffer, std::size_t size) = 0;
 };
