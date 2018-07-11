82dc6df785d310486d3bc770b6f09756dadae3ab
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-07-10 16:31:01 -1000

    Update OP2Utility.h to include all pertinent header files

--- include/OP2Utility.h
+++ include/OP2Utility.h
@@ -1,14 +1,22 @@
 #pragma once
 
-#include "StringHelper.h"
-#include "Streams/StreamReader.h"
-#include "XFile.h"
-#include "Maps/MapData.h"
-
-#include "ResourceManager.h"
 #include "Archives/ArchiveFile.h"
 #include "Archives/ClmFile.h"
 #include "Archives/VolFile.h"
+
+#include "Maps/MapData.h"
+#include "Maps/MapReader.h"
+#include "Maps/MapWriter.h"
+
+#include "Streams/FileStreamReader.h"
+#include "Streams/FileSliceReader.h"
+#include "Streams/FileStreamWriter.h"
+#include "Streams/MemoryStreamReader.h"
+#include "Streams/MemoryStreamWriter.h"
+
+#include "ResourceManager.h"
+#include "StringHelper.h"
+#include "XFile.h"
 
 // OP2Utility is a utility library to aid in Outpost 2 functions typically not
 // directly related to programming new scenarios. 
