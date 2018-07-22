8feb7efa4db6f2b4afde82ea67589ecff07ae896
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-07-23 03:03:44 +0700

    Implement stream copy
    Allow a StreamWriter to Write the entire contents from a StreamReader.

--- src/Streams/StreamWriter.h
+++ src/Streams/StreamWriter.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "StreamReader.h"
+
 #include <cstddef>
 #include <type_traits>
 #include <vector>
+#include <array>
 
 class StreamWriter {
 protected:
@@ -31,5 +34,19 @@ public:
 	template<typename T, typename A>
 	inline void Write(const std::vector<T, A>& vector) {
 		WriteImplementation(vector.data(), vector.size() * sizeof(T));
+	}
+
+	// Copy a StreamReader to a StreamWriter
+	static const std::size_t DefaultCopyChunkSize = 0x00020000;
+	template<std::size_t BufferSize = DefaultCopyChunkSize>
+	void Write(StreamReader& streamReader) {
+		std::array<char, BufferSize> buffer;
+		std::size_t numBytesRead;
+
+		do {
+			// Read the input stream
+			numBytesRead = streamReader.ReadPartial(buffer.data(), BufferSize);
+			Write(buffer.data(), numBytesRead);
+		} while (numBytesRead); // End loop when numBytesRead/Written is equal to 0
 	}
 };
