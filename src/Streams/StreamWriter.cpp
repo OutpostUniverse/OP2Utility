ee714fa30b9549a366867cdd65f13323deac469d
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-07-23 18:10:36 +0700

    Move stream copy implementation to .cpp file

---
+++ src/Streams/StreamWriter.cpp
@@ -0,0 +1,16 @@
+
+#include "StreamWriter.h"
+#include "StreamReader.h"
+#include <array>
+
+template<std::size_t BufferSize>
+void StreamWriter::Write(StreamReader& streamReader) {
+  std::array<char, BufferSize> buffer;
+  std::size_t numBytesRead;
+
+  do {
+    // Read the input stream
+    numBytesRead = streamReader.ReadPartial(buffer.data(), BufferSize);
+    Write(buffer.data(), numBytesRead);
+  } while (numBytesRead); // End loop when numBytesRead/Written is equal to 0
+}
