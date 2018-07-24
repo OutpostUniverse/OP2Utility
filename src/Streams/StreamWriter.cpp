898c5e41cabbdc65a95e5bb501e4d8040d01d41c
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-07-25 01:28:09 +0700

    Convert spaces to tabs

--- src/Streams/StreamWriter.cpp
+++ src/Streams/StreamWriter.cpp
@@ -5,12 +5,12 @@
 
 template<std::size_t BufferSize>
 void StreamWriter::Write(StreamReader& streamReader) {
-  std::array<char, BufferSize> buffer;
-  std::size_t numBytesRead;
+	std::array<char, BufferSize> buffer;
+	std::size_t numBytesRead;
 
-  do {
-    // Read the input stream
-    numBytesRead = streamReader.ReadPartial(buffer.data(), BufferSize);
-    Write(buffer.data(), numBytesRead);
-  } while (numBytesRead); // End loop when numBytesRead/Written is equal to 0
+	do {
+		// Read the input stream
+		numBytesRead = streamReader.ReadPartial(buffer.data(), BufferSize);
+		Write(buffer.data(), numBytesRead);
+	} while (numBytesRead); // End loop when numBytesRead/Written is equal to 0
 }
