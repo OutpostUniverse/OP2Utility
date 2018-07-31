e182bb8fd0e67e6c22accd847d48883197f3cafb
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-07-30 16:47:35 -1000

    Switch to using suppress pre-processor directive instead of disable

--- src/Archives/ClmFile.cpp
+++ src/Archives/ClmFile.cpp
@@ -273,13 +273,11 @@ namespace Archives
 		uint32_t offset = headerSize + internalNames.size() * sizeof(IndexEntry);
 		for (std::size_t i = 0; i < internalNames.size(); ++i)
 		{
-#pragma warning( push )
-#pragma warning( disable : 4996 ) // Disable MSVC warning 'strncpy': This function or variable may be unsafe. Consider using strncpy_s instead.
-			// As of 28Jul18, strncpy_s was not available on Linux.
-			
+			// Disable MSVC warning 'strncpy': This function or variable may be unsafe. Consider using strncpy_s instead.
+			// As of 28Jul18, strncpy_s was not available on Linux.		
 			// Copy the filename into the entry
+#pragma warning( suppress : 4996 )
 			std::strncpy(indexEntries[i].filename.data(), internalNames[i].data(), sizeof(IndexEntry::filename));
-#pragma warning ( pop )
 
 			// Set the offset of the file
 			indexEntries[i].dataOffset = offset;
