8a8c9fc3f40eb86b3f08634b474f18154d596a1c
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-08-06 17:52:14 -0700

    Add TilesetSource.h

---
+++ src/Maps/TilesetSource.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+#pragma pack(push, 1) // Make sure structures are byte aligned
+
+// Facilitates finding the source BMP file (well00XX.bmp) for a tile set.
+// Tile set names must be exactly 8 chars long not including file extension.
+struct TilesetSource
+{
+	// Fixed length string. Map and save files require tile set filenames to be exactly 8
+	// characters long. User must handle terminating the string.
+	std::string tilesetFilename;
+
+	// Number of Tiles in set (represented on BMP).
+	uint32_t numTiles;
+};
+
+#pragma pack(pop)
