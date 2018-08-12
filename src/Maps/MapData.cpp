41e1809c9aff25f87b16bf892d21ecfb53d0bd60
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-08-12 21:43:04 +0700

    Use `std::size_t` for indexes in MapData

--- src/Maps/MapData.cpp
+++ src/Maps/MapData.cpp
@@ -1,37 +1,37 @@
 #include "MapData.h"
 #include "CellType.h"
 
-unsigned int MapData::GetTileInfoIndex(unsigned int x, unsigned int y) const
+unsigned int MapData::GetTileInfoIndex(std::size_t x, std::size_t y) const
 {
 	std::size_t cellIndex = GetCellIndex(x, y);
 	return tiles[cellIndex].tileIndex;
 }
 
-CellType MapData::GetCellType(unsigned int x, unsigned int y) const
+CellType MapData::GetCellType(std::size_t x, std::size_t y) const
 {
 	return tiles[GetCellIndex(x, y)].cellType;
 }
 
-int MapData::GetLavaPossible(unsigned int x, unsigned int y) const
+int MapData::GetLavaPossible(std::size_t x, std::size_t y) const
 {
 	return tiles[GetCellIndex(x, y)].bLavaPossible;
 }
 
-uint16_t MapData::GetTilesetIndex(unsigned int x, unsigned int y) const
+uint16_t MapData::GetTilesetIndex(std::size_t x, std::size_t y) const
 {
 	unsigned int tileInfoIndex = GetTileInfoIndex(x, y);
 	return tileInfos[tileInfoIndex].tilesetIndex;
 }
 
-uint16_t MapData::GetImageIndex(unsigned int x, unsigned int y) const
+uint16_t MapData::GetImageIndex(std::size_t x, std::size_t y) const
 {
 	unsigned int tileInfoIndex = GetTileInfoIndex(x, y);
 	return tileInfos[tileInfoIndex].tileIndex;
 }
 
-std::size_t MapData::GetCellIndex(unsigned int x, unsigned int y) const
+std::size_t MapData::GetCellIndex(std::size_t x, std::size_t y) const
 {
-	uint32_t lowerX = x & 0x1F; // ... 0001 1111
-	uint32_t upperX = x >> 5;   // ... 1110 0000
+	std::size_t lowerX = x & 0x1F; // ... 0001 1111
+	std::size_t upperX = x >> 5;   // ... 1110 0000
 	return (upperX * header.mapTileHeight + y) * 32 + lowerX;
 }
