8f6db873b4c0cc100941c624abb6af00a19bb664
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-08-14 19:24:04 -0700

    Fix typo in Read function comment

--- src/Streams/StreamWriter.h
+++ src/Streams/StreamWriter.h
@@ -59,7 +59,7 @@ public:
 
 	// Size prefixed string data types
 	// Ex: Write<uint32_t>(string); // Write 32-bit string length followed by string data
-	// Does not write null terminator unless specifcally included in string.
+	// Does not write null terminator unless specifically included in string.
 	template<typename SizeType, typename CharT, typename Traits, typename Allocator>
 	void Write(const std::basic_string<CharT, Traits, Allocator>& string) {
 		auto stringSize = string.size();
