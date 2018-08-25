4c55f396e727e71c5fb840aaab3a7925bae3406d
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-08-25 11:17:12 +0700

    Convert space indents to tabs

--- test/Archives/AdaptiveHuffmanTree.test.cpp
+++ test/Archives/AdaptiveHuffmanTree.test.cpp
@@ -3,20 +3,20 @@
 
 
 TEST(AdaptiveHuffmanTreeTests, SimpleTree2) {
-  // Construct minimum non-degenerate binary tree with 2 data nodes
-  Archives::AdaptiveHuffmanTree tree(2);
+	// Construct minimum non-degenerate binary tree with 2 data nodes
+	Archives::AdaptiveHuffmanTree tree(2);
 
-  // With 2 data nodes, the root can not be a data node
-  auto rootIndex = tree.GetRootNodeIndex();
-  ASSERT_FALSE(tree.IsLeaf(rootIndex));
+	// With 2 data nodes, the root can not be a data node
+	auto rootIndex = tree.GetRootNodeIndex();
+	ASSERT_FALSE(tree.IsLeaf(rootIndex));
 
-  // With 2 data nodes, the children of the root must be data nodes
-  auto leftChild = tree.GetChildNode(rootIndex, false);
-  auto rightChild = tree.GetChildNode(rootIndex, true);
-  ASSERT_TRUE(tree.IsLeaf(leftChild));
-  ASSERT_TRUE(tree.IsLeaf(rightChild));
+	// With 2 data nodes, the children of the root must be data nodes
+	auto leftChild = tree.GetChildNode(rootIndex, false);
+	auto rightChild = tree.GetChildNode(rootIndex, true);
+	ASSERT_TRUE(tree.IsLeaf(leftChild));
+	ASSERT_TRUE(tree.IsLeaf(rightChild));
 
-  // No tree updates, so initial data should still be in order
-  ASSERT_EQ(0, tree.GetNodeData(leftChild));
-  ASSERT_EQ(1, tree.GetNodeData(rightChild));
+	// No tree updates, so initial data should still be in order
+	ASSERT_EQ(0, tree.GetNodeData(leftChild));
+	ASSERT_EQ(1, tree.GetNodeData(rightChild));
 }
