ed1fe7c560b3f314c5626a4f17448882b71f9e96
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-08-24 21:26:20 -0400

    Add more MemoryStreamReader tests
     - Seeking out of bounds
     - Reading out of bounds
     - Length of Stream
     - Position of Stream

--- test/Streams/MemoryStreamReader.test.cpp
+++ test/Streams/MemoryStreamReader.test.cpp
@@ -1,6 +1,6 @@
 #include "Streams/MemoryReader.h"
 #include <gtest/gtest.h>
-
+#include <array>
 
 // Simple test
 
@@ -38,4 +38,41 @@ TEST_F(EmptyMemoryStreamReader, EmptySize) {
   // Read 0 bytes
   ASSERT_NO_THROW(stream.Read(nullptr, 0));
   EXPECT_EQ(0, stream.ReadPartial(nullptr, 0));
+}
+
+// Setup fixture
+class IntMemoryStreamReader : public ::testing::Test {
+public:
+	IntMemoryStreamReader() : stream(buffer.data(), buffer.size()) {}
+protected:
+	Stream::MemoryReader stream;
+	const std::array<int, 5> buffer{ 0, 1, 2, 3, 4 };
+};
+
+TEST_F(IntMemoryStreamReader, SeekOutOfBounds) {
+	EXPECT_THROW(stream.Seek(6), std::runtime_error);
+}
+
+TEST_F(IntMemoryStreamReader, ReadOutOfBounds) {
+	std::array<int, 6> destinationBuffer;
+	EXPECT_THROW(stream.Read(destinationBuffer), std::runtime_error);
+}
+
+TEST_F(IntMemoryStreamReader, SeekOutOfBoundsRelative) {
+	// Seeking out of bounds should return a Stream::Seekable to previous condition
+	EXPECT_THROW(stream.SeekRelative(6), std::runtime_error);
+	EXPECT_THROW(stream.SeekRelative(-1), std::runtime_error);
+}
+
+TEST_F(IntMemoryStreamReader, StreamSize) {
+	EXPECT_EQ(stream.Length(), buffer.size());
+}
+
+TEST_F(IntMemoryStreamReader, StreamPosition) {
+	std::array<int, 6> destinationBuffer;
+
+	EXPECT_EQ(stream.Position(), 0);
+
+	stream.Read(&destinationBuffer[0], 1);
+	EXPECT_EQ(stream.Position(), 1);
 }
