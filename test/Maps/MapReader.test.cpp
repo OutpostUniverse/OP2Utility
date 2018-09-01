a8fd2d002ace585995489cfd6ba5fe3fc93efd97
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-09-01 16:25:27 -0400

    Update to SavedGame in Test

--- test/Maps/MapReader.test.cpp
+++ test/Maps/MapReader.test.cpp
@@ -5,14 +5,14 @@
 
 TEST(MapReader, MissingFile) {
 	EXPECT_THROW(MapReader::Read("MissingFile.map", MapType::Map), std::runtime_error);
-	EXPECT_THROW(MapReader::Read("MissingFile.op2", MapType::Save), std::runtime_error);
+	EXPECT_THROW(MapReader::Read("MissingFile.op2", MapType::SavedGame), std::runtime_error);
 
 	// Check if filename is an empty string
 	EXPECT_THROW(MapReader::Read(std::string(""), MapType::Map), std::runtime_error);
-	EXPECT_THROW(MapReader::Read(std::string(""), MapType::Save), std::runtime_error);
+	EXPECT_THROW(MapReader::Read(std::string(""), MapType::SavedGame), std::runtime_error);
 }
 
 TEST(MapReader, EmptyFile) {
 	EXPECT_THROW(MapReader::Read("Maps/data/EmptyMap.map", MapType::Map), std::runtime_error);
-	EXPECT_THROW(MapReader::Read("Maps/data/EmptySave.OP2", MapType::Save), std::runtime_error);
+	EXPECT_THROW(MapReader::Read("Maps/data/EmptySave.OP2", MapType::SavedGame), std::runtime_error);
 }
