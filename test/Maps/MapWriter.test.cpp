92215d319b34b928fd089947b5a3abcaeb1349ec
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-09-02 15:58:33 +0700

    Construct empty MapData inline

--- test/Maps/MapWriter.test.cpp
+++ test/Maps/MapWriter.test.cpp
@@ -7,24 +7,20 @@
 
 TEST(MapWriter, EmptyMapData)
 {
-	MapData mapData;
-	
 	// Write to File
 	const std::string testFilename("maps/data/test.map");
-	EXPECT_THROW(MapWriter::Write(testFilename, mapData), std::runtime_error);
+	EXPECT_THROW(MapWriter::Write(testFilename, MapData()), std::runtime_error);
 	XFile::DeletePath(testFilename);
 
 	// Write to Memory
 	std::vector<char> buffer(1024);
 	Stream::MemoryWriter memoryWriter(buffer.data(), buffer.size() * sizeof(decltype(buffer)::value_type));
-	EXPECT_THROW(MapWriter::Write(memoryWriter, mapData), std::runtime_error);
+	EXPECT_THROW(MapWriter::Write(memoryWriter, MapData()), std::runtime_error);
 }
 
 TEST(MapWriter, BlankFilename)
 {
-	MapData mapData;
-
-	EXPECT_THROW(MapWriter::Write("", mapData), std::runtime_error);
+	EXPECT_THROW(MapWriter::Write("", MapData()), std::runtime_error);
 }
 
 TEST(MapWriter, VersionTag) {
