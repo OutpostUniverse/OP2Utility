fdfd2dff155429fae48ad783f9457e6a676a005a
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-09-17 08:47:18 -0400

    Separate BmpWriter 8bit and monochrome tests into separate macros

--- test/Sprites/BmpWriter.test.cpp
+++ test/Sprites/BmpWriter.test.cpp
@@ -21,13 +21,21 @@ TEST(IndexedBmpWriter, WriteMonochrome) {
 	pixels = std::vector<uint8_t>{ 255, 255, 255, 255, 255, 0, 0, 0, 0, 0 };
 	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 40, -2, palette, pixels));
 
-	pixels = std::vector<uint8_t>{ 0, 0, 0, 1, 1, 1 };
-	std::vector<Color> palette8Bit;
-	palette8Bit.resize(256);
-	std::fill(palette8Bit.begin(), palette8Bit.end(), DiscreteColor::Blue);
-	palette8Bit[0] = DiscreteColor::Red;
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 3, -2, palette8Bit, pixels));
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 3, 2, palette8Bit, pixels));
+	XFile::DeletePath(filename);
+}
 
-	//XFile::DeletePath(filename);
+TEST(IndexedBmpWriter, Write8Bit)
+{
+	std::string filename = "Sprites/data/8BitTest.bmp";
+
+	std::vector<uint8_t> pixels{ 0, 0, 0, 1, 1, 1 };
+	std::vector<Color> palette;
+	palette.resize(256);
+	std::fill(palette.begin(), palette.end(), DiscreteColor::Blue);
+	palette[0] = DiscreteColor::Red;
+	
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 3, -2, palette, pixels));
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 3, 2, palette, pixels));
+
+	XFile::DeletePath(filename);
 }
