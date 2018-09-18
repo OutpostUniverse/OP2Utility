bf1083075407007ec6a5729c46f0bb701e875371
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-09-18 22:13:25 +0700

    Remove unused bounds check method
    Note: We also lose reporting of the archive file name by not using a
    custom method.

--- src/Archives/ArchiveUnpacker.cpp
+++ src/Archives/ArchiveUnpacker.cpp
@@ -54,11 +54,4 @@ namespace Archives
 	{
 		return OpenStream(GetIndex(name));
 	}
-
-	void ArchiveUnpacker::VerifyIndexInBounds(std::size_t index)
-	{
-		if (index >= m_Count) {
-			throw std::runtime_error("Index " + std::to_string(index) + " is out of bounds in archive " + m_ArchiveFilename + ".");
-		}
-	}
 }
