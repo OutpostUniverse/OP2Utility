1c8de305cf135f72bbc576b3a976a60398eae18b
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-09-20 11:35:12 -0400

    Add ImageHeader::Create Unit Test

--- test/Sprites/ImageHeader.test.cpp
+++ test/Sprites/ImageHeader.test.cpp
@@ -3,6 +3,31 @@
 #include <stdexcept>
 #include <cstdint>
 
+TEST(ImageHeader, Create)
+{
+	const int32_t width = 10;
+	const int32_t height = 10;
+	const uint16_t bitCount = 1;
+
+	// Improper Bit Count throws
+	EXPECT_THROW(ImageHeader::Create(width, height, 3), std::runtime_error);
+
+	ImageHeader imageHeader;
+	EXPECT_NO_THROW(imageHeader = ImageHeader::Create(width, height, bitCount));
+
+	EXPECT_EQ(sizeof(ImageHeader), imageHeader.headerSize);
+	EXPECT_EQ(width, imageHeader.width);
+	EXPECT_EQ(height, imageHeader.height);
+	EXPECT_EQ(ImageHeader::defaultPlanes, imageHeader.defaultPlanes);
+	EXPECT_EQ(bitCount, imageHeader.bitCount);
+	EXPECT_EQ(ImageHeader::defaultCompression, imageHeader.compression);
+	EXPECT_EQ(ImageHeader::defaultImageSize, imageHeader.defaultImageSize);
+	EXPECT_EQ(ImageHeader::defaultXResolution, imageHeader.defaultXResolution);
+	EXPECT_EQ(ImageHeader::defaultYResolution, imageHeader.defaultYResolution);
+	EXPECT_EQ(ImageHeader::defaultUsedColorMapEntries, imageHeader.defaultUsedColorMapEntries);
+	EXPECT_EQ(ImageHeader::defaultImportantColorCount, imageHeader.defaultImportantColorCount);
+}
+
 TEST(ImageHeader, IsBitCount)
 {
 	for (auto bitCount : ImageHeader::validBitCounts) {
