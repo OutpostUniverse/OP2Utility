7ed4a48f05277a38066d05ed9c1816582653f776
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-09-25 20:31:00 -0400

    Remove non-padded Pixel Write Function

--- src/Sprites/IndexedBmpWriter.h
+++ src/Sprites/IndexedBmpWriter.h
@@ -11,32 +11,19 @@
 enum class BitCount : uint16_t;
 
 namespace Stream {
-	class SeekableReader;
 	class SeekableWriter;
 }
 
 // BMP Writer only supporting Indexed Color palettes (1, 2, and 8 bit BMPs). 
-// Properly writes BMPs with pixels in Bottom Up format (positive height) and Top Down format (negative height)
 class IndexedBmpWriter
 {
 public:
-	// The pixel container includes dummy information to fill each image row out to the next 4 byte memory border.
-	// Only supports a 4 byte pitch.
-	static void WritePixelsIncludingPadding(std::string filename, uint16_t bitCount, int32_t width, int32_t height, const std::vector<Color>& palette, const std::vector<uint8_t>& pixelsWithPadding);
-
-	// Dummy information is inserted at the end of each pixel row to reach the next 4 byte memory border
+	// @indexedPixels: Must include padding to fill each image row out to the next 4 byte memory border (pitch).
 	static void Write(std::string filename, uint16_t bitCount, int32_t width, int32_t height, const std::vector<Color>& palette, const std::vector<uint8_t>& indexedPixels);
+
 
 private:
 	static void WriteHeaders(Stream::SeekableWriter& seekableWriter, uint16_t bitCount, int width, int height, const std::vector<Color>& palette);
-
-	static void WritePixels(Stream::SeekableWriter& seekableWriter, uint16_t bitCount, int32_t width, int32_t height, const std::vector<uint8_t>& pixels);
-
-	// If height is a negative number, pixel rows are written top down (as displayed on screen)
-	static void WritePixelsTopDown(Stream::SeekableWriter& fileWriter, uint16_t bitCount, int32_t width, int32_t height, const std::vector<uint8_t>& pixels);
-
-	// If height is a positive number, the top row of pixels is written at the end of the file 
-	static void WritePixelsBottomUp(Stream::SeekableWriter& fileWriter, uint16_t bitCount, int32_t width, int32_t height, const std::vector<uint8_t>& pixels);
 
 	static unsigned int CalculatePitch(uint16_t bitCount, int32_t width);
 
@@ -44,7 +31,6 @@ private:
 	static unsigned int CalcPixelByteWidth(uint16_t bitCount, int32_t width);
 
 	static void VerifyPaletteSizeDoesNotExceedBitCount(uint16_t bitCount, std::size_t paletteSize);
-	static void VerifyPixelBufferSizeMatchesImageDimensions(uint16_t bitCount, std::size_t pixelCount, std::size_t pixelArraySize);
 
 	// Check the pixel count is correct if it already includes dummy pixels out to next 4 byte boundary.
 	// @width: Width in pixels. Do not include the pitch in width.
--- test/Sprites/IndexedBmpWriter.test.cpp
+++ test/Sprites/IndexedBmpWriter.test.cpp
@@ -8,22 +8,19 @@
 
 TEST(IndexedBmpWriter, WriteMonochrome) 
 {
-	std::vector<Color> palette{ DiscreteColor::Red, DiscreteColor::Blue };
+	std::vector<Color> palette{ DiscreteColor::Black, DiscreteColor::White };
 	std::string filename = "Sprites/data/MonochromeTest.bmp";
 
-	std::vector<uint8_t> pixels { 255, 255, 255, 0, 0, 0 };
+	std::vector<uint8_t> pixels{ 128, 0, 0, 0, 128 , 0, 0, 0};
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 1, 2, palette, pixels));
 
-	// Test width 1 byte less than scan line
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 24, -2, palette, pixels));
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 24, 2, palette, pixels));
+	// Test padding 1 byte less than pitch
+	pixels = std::vector<uint8_t>{ 128, 0, 0, 128, 0, 0 };
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 1, 1, 2, palette, pixels), std::runtime_error);
 
-	// Test width equal to scan line
-	pixels = std::vector<uint8_t>{ 255, 255, 255, 255, 0, 0, 0, 0 };
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 32, -2, palette, pixels));
-
-	// Test width 1 byte greater than scan line
-	pixels = std::vector<uint8_t>{ 255, 255, 255, 255, 255, 0, 0, 0, 0, 0 };
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 40, -2, palette, pixels));
+	// Test padding 1 byte greater than pitch
+	pixels = std::vector<uint8_t>{ 128, 0, 0, 0, 0, 128 , 0, 0, 0, 0 };
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 1, 1, 2, palette, pixels), std::runtime_error);
 
 	XFile::DeletePath(filename);
 }
@@ -31,12 +28,19 @@ TEST(IndexedBmpWriter, WriteMonochrome)
 TEST(IndexedBmpWriter, Write4Bit)
 {
 	std::string filename = "Sprites/data/4BitTest.bmp";
-	std::vector<uint8_t> pixels{ 0, 0, 0b00010001,  0b00010001 };
-	std::vector<Color> palette(16, DiscreteColor::Blue);
-	palette[0] = DiscreteColor::Red;
+	std::vector<uint8_t> pixels{ 0, 0, 0, 0, 0, 0, 0, 0 };
+	std::vector<Color> palette(16, DiscreteColor::Black);
+	palette[0] = DiscreteColor::White;
 
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 4, 4, -2, palette, pixels));
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 4, 4, 2, palette, pixels));
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 4, 1, 2, palette, pixels));
+
+	// Test padding 1 byte less than pitch
+	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0 };
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
+
+	// Test padding 1 byte greater than pitch
+	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
 
 	XFile::DeletePath(filename);
 }
@@ -45,12 +49,19 @@ TEST(IndexedBmpWriter, Write8Bit)
 {
 	std::string filename = "Sprites/data/8BitTest.bmp";
 
-	std::vector<uint8_t> pixels{ 0, 0, 0, 1, 1, 1 };
-	std::vector<Color> palette(256, DiscreteColor::Blue);
-	palette[0] = DiscreteColor::Red;
+	std::vector<uint8_t> pixels{ 0, 0, 0, 0, 0, 0, 0, 0 };
+	std::vector<Color> palette(256, DiscreteColor::Black);
+	palette[0] = DiscreteColor::White;
 	
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 3, -2, palette, pixels));
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 3, 2, palette, pixels));
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels));
+
+	// Test padding 1 byte less than pitch
+	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0 };
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
+
+	// Test padding 1 byte greater than pitch
+	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
 
 	XFile::DeletePath(filename);
 }
