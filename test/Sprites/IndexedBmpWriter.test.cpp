b88ce701b5a6e6f286e9bb36624a6b4a30c52ea9
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-09-26 21:34:26 -0400

    Rewrite IndexedBmpWriter test suite

--- test/Sprites/IndexedBmpWriter.test.cpp
+++ test/Sprites/IndexedBmpWriter.test.cpp
@@ -6,62 +6,56 @@
 #include <vector>
 #include <cstdint>
 
-TEST(IndexedBmpWriter, WriteMonochrome) 
+TEST(IndexedBmpWriter, Write)
 {
-	std::vector<Color> palette{ DiscreteColor::Black, DiscreteColor::White };
-	std::string filename = "Sprites/data/MonochromeTest.bmp";
+	const std::string filename = "Sprites/data/BitmapTest.bmp";
+	const std::vector<uint8_t> pixels(4, 0);
 
-	std::vector<uint8_t> pixels{ 128, 0, 0, 0, 128 , 0, 0, 0};
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 1, 2, palette, pixels));
+	std::vector<Color> palette(2);
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 1, 1, 1, palette, pixels));
 
-	// Test padding 1 byte less than pitch
-	pixels = std::vector<uint8_t>{ 128, 0, 0, 128, 0, 0 };
-	EXPECT_THROW(IndexedBmpWriter::Write(filename, 1, 1, 2, palette, pixels), std::runtime_error);
+	palette.resize(16);
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 4, 1, 1, palette, pixels));
 
-	// Test padding 1 byte greater than pitch
-	pixels = std::vector<uint8_t>{ 128, 0, 0, 0, 0, 128 , 0, 0, 0, 0 };
-	EXPECT_THROW(IndexedBmpWriter::Write(filename, 1, 1, 2, palette, pixels), std::runtime_error);
+	palette.resize(256);
+	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 1, 1, palette, pixels));
 
 	XFile::DeletePath(filename);
 }
 
-TEST(IndexedBmpWriter, Write4Bit)
+TEST(IndexedBmpWriter, InvalidBitCount)
 {
-	std::string filename = "Sprites/data/4BitTest.bmp";
-	std::vector<uint8_t> pixels{ 0, 0, 0, 0, 0, 0, 0, 0 };
-	std::vector<Color> palette(16, DiscreteColor::Black);
-	palette[0] = DiscreteColor::White;
+	const std::vector<Color> palette(8);
+	const std::vector<uint8_t> pixels(4, 0);
+	const std::string filename = "Sprites/data/MonochromeTest.bmp";
 
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 4, 1, 2, palette, pixels));
-
-	// Test padding 1 byte less than pitch
-	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0 };
-	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
-
-	// Test padding 1 byte greater than pitch
-	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 3, 1, 1, palette, pixels), std::runtime_error);
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 32, 1, 1, palette, pixels), std::runtime_error);
 
 	XFile::DeletePath(filename);
 }
 
-TEST(IndexedBmpWriter, Write8Bit)
+TEST(IndexedBmpWriter, TooManyPaletteEntries)
 {
-	std::string filename = "Sprites/data/8BitTest.bmp";
+	const std::vector<Color> palette(3);
+	const std::vector<uint8_t> pixels(4, 0);
+	const std::string filename("Sprites/data/PaletteRangeTest.bmp");
 
-	std::vector<uint8_t> pixels{ 0, 0, 0, 0, 0, 0, 0, 0 };
-	std::vector<Color> palette(256, DiscreteColor::Black);
-	palette[0] = DiscreteColor::White;
-	
-	EXPECT_NO_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels));
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 1, 1, 1, palette, pixels), std::runtime_error);
 
-	// Test padding 1 byte less than pitch
-	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0 };
-	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
+	XFile::DeletePath(filename);
+}
 
-	// Test padding 1 byte greater than pitch
-	pixels = std::vector<uint8_t>{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	EXPECT_THROW(IndexedBmpWriter::Write(filename, 8, 1, 2, palette, pixels), std::runtime_error);
+TEST(IndexedBmpWriter, IncorrectPixelPadding)
+{
+	const std::vector<Color> palette(2);
+	const std::string filename("Sprites/data/IncorrectPixelPaddingTest.bmp");
+
+	std::vector<uint8_t> pixels(3, 0);
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 1, 1, 1, palette, pixels), std::runtime_error);
+
+	pixels.resize(5, 0);
+	EXPECT_THROW(IndexedBmpWriter::Write(filename, 1, 1, 1, palette, pixels), std::runtime_error);
 
 	XFile::DeletePath(filename);
 }
