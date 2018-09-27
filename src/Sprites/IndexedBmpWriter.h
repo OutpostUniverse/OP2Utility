4052704ecc2bbcd500bdd8910396ed44d8805557
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-09-27 18:07:33 -0400

    Allow partially filled palettes
     - Fill palette to capacity with Black (0, 0, 0, 0)
     - Remove check to ensure each pixel is a valid palette entry
    
    Closes #201

--- src/Sprites/IndexedBmpWriter.h
+++ src/Sprites/IndexedBmpWriter.h
@@ -17,7 +17,7 @@ class IndexedBmpWriter
 {
 public:
 	// @indexedPixels: Must include padding to fill each image row out to the next 4 byte memory border (pitch).
-	static void Write(std::string filename, uint16_t bitCount, int32_t width, int32_t height, const std::vector<Color>& palette, const std::vector<uint8_t>& indexedPixels);
+	static void Write(std::string filename, uint16_t bitCount, int32_t width, int32_t height, std::vector<Color> palette, const std::vector<uint8_t>& indexedPixels);
 
 
 private:
@@ -31,6 +31,4 @@ private:
 	// @width: Width in pixels. Do not include the pitch in width.
 	// @pixelsWithPitchSize: Number of pixels including padding pixels to next 4 byte boundary.
 	static void VerifyPixelBufferSizeMatchesImageDimensionsWithPitch(uint16_t bitCount, int32_t width, int32_t height, std::size_t pixelsWithPitchSize);
-
-	static void VerifyPixelsContainedInPalette(uint16_t bitCount, std::size_t paletteEntryCount, const std::vector<uint8_t>& pixels);
 };
