f9827fea6c3b7489ec3c5c1bc7222ab718d6082a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-10-01 00:17:58 +0700

    Test equality against constructed values
    This protects against trivial implementations that compare based on
    identity, rather than equivalence. That is, implementations that
    compare based on the address of the color struct, rather than the values
    of the fields.
    
    Example bad implementation:
    ```cpp
    bool operator==(const Color& lhs, const Color& rhs) {
      // Oh no! A bug! :o
      return &lhs == &rhs;
    }
    ```
    
    Such an implementation would sneak past the previous tests, where
    equality happened to coincide with identity. That is, there were no two
    distinct Color objects with identical color values.

--- test/Sprites/Color.test.cpp
+++ test/Sprites/Color.test.cpp
@@ -16,3 +16,13 @@ TEST(Color, CompareEquality) {
 	// Check negative result
 	EXPECT_FALSE(DiscreteColor::Black == DiscreteColor::White);
 }
+
+TEST(Color, CompareEqualityWithNewlyConstructedValue) {
+	// Construct these non-inline so as to not interfere with macro expansion
+	// (Preprocessor gets confused when C++ grammar ","s appear on the same line)
+	const Color Black{ 0, 0, 0, 0 };
+	const Color White{ 255, 255, 255, 0 };
+	// Colors compare equal with constructed equivalents
+	EXPECT_EQ(DiscreteColor::Black, Black);
+	EXPECT_EQ(DiscreteColor::White, White);
+}
