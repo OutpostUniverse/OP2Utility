7af436baf1d219467f3955eca322734b656cda76
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-10-01 00:41:08 +0700

    Add secondary color constants
    I had originally added these for testing of 1 component differences,
    though later realized the primary colors differ from black by 1
    component. The color constants might still be of use though.

--- src/Sprites/Color.h
+++ src/Sprites/Color.h
@@ -25,7 +25,12 @@ namespace DiscreteColor
 {
 	const Color Black{ 0, 0, 0, 0 };
 	const Color White{ 255, 255, 255,0 };
+
 	const Color Red{ 255, 0, 0, 0 };
 	const Color Green{ 0, 255, 0, 0 };
 	const Color Blue{ 0, 0, 255, 0 };
+
+	const Color Yellow{ 255, 255, 0 };
+	const Color Cyan{ 0, 255, 255 };
+	const Color Magenta{ 255, 0, 255 };
 }
