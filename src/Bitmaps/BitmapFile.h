0ded0137e2624afd71606010a1017d71c0337c8e
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-10-07 20:49:08 -0400

    Add member variables to BitmapFile
     - Move functions from IndexedBmpWriter.cpp into BitmapFile that should be available outside of writing bitmaps

--- src/Bitmaps/BitmapFile.h
+++ src/Bitmaps/BitmapFile.h
@@ -3,7 +3,6 @@
 #include "Color.h"
 #include "BmpHeader.h"
 #include "ImageHeader.h"
-#include <array>
 #include <vector>
 #include <string>
 #include <cstdint>
@@ -17,21 +16,28 @@ namespace Stream {
 class BitmapFile
 {
 public:
+	BmpHeader bmpHeader;
+	ImageHeader imageHeader;
+	std::vector<Color> palette;
+	std::vector<uint8_t> pixels;
+
+
 	// @indexedPixels: Must include padding to fill each image row out to the next 4 byte memory border (pitch).
 	static void WriteIndexed(std::string filename, uint16_t bitCount, int32_t width, int32_t height, std::vector<Color> palette, const std::vector<uint8_t>& indexedPixels);
 	static void WriteIndexed(Stream::SeekableWriter& seekableWriter, uint16_t bitCount, int32_t width, int32_t height, std::vector<Color> palette, const std::vector<uint8_t>& indexedPixels);
 
-private:
-	static void VerifyIndexedImage(uint16_t bitCount);
+	void VerifyIndexedPaletteSizeDoesNotExceedBitCount() const;
+	static void VerifyIndexedPaletteSizeDoesNotExceedBitCount(uint16_t bitCount, std::size_t paletteSize);
 
-	static void WriteHeaders(Stream::SeekableWriter& seekableWriter, uint16_t bitCount, int width, int height, const std::vector<Color>& palette);
-
-	static void WritePixels(Stream::SeekableWriter& seekableWriter, const std::vector<uint8_t>& pixels, int32_t width, uint16_t bitCount);
-
-	static void VerifyPaletteSizeDoesNotExceedBitCount(uint16_t bitCount, std::size_t paletteSize);
-
-	// Check the pixel count is correct if it already includes dummy pixels out to next 4 byte boundary.
+	// Check the pixel count is correct and already includes dummy pixels out to next 4 byte boundary.
 	// @width: Width in pixels. Do not include the pitch in width.
 	// @pixelsWithPitchSize: Number of pixels including padding pixels to next 4 byte boundary.
+	void VerifyPixelSizeMatchesImageDimensionsWithPitch() const;
 	static void VerifyPixelSizeMatchesImageDimensionsWithPitch(uint16_t bitCount, int32_t width, int32_t height, std::size_t pixelsWithPitchSize);
+
+private:
+	// Write
+	static void VerifyIndexedImageForWriting(uint16_t bitCount);
+	static void WriteHeaders(Stream::SeekableWriter& seekableWriter, uint16_t bitCount, int width, int height, const std::vector<Color>& palette);
+	static void WritePixels(Stream::SeekableWriter& seekableWriter, const std::vector<uint8_t>& pixels, int32_t width, uint16_t bitCount);
 };
---
+++ test/Bitmaps/BitmapFile.test.cpp
@@ -0,0 +1,32 @@
+#include "../src/Bitmaps/BitmapFile.h"
+#include <gtest/gtest.h>
+#include <stdexcept>
+
+TEST(BitmapFile, VerifyIndexedPaletteSizeDoesNotExceedBitCount)
+{
+	EXPECT_NO_THROW(BitmapFile::VerifyIndexedPaletteSizeDoesNotExceedBitCount(1, 1));
+	EXPECT_NO_THROW(BitmapFile::VerifyIndexedPaletteSizeDoesNotExceedBitCount(1, 2));
+	EXPECT_THROW(BitmapFile::VerifyIndexedPaletteSizeDoesNotExceedBitCount(1, 3), std::runtime_error);
+
+	// Test non-static version of function
+	BitmapFile bitmapFile;
+	bitmapFile.imageHeader = ImageHeader::Create(1, 1, 1);
+	bitmapFile.palette.resize(2);
+	EXPECT_NO_THROW(bitmapFile.VerifyIndexedPaletteSizeDoesNotExceedBitCount());
+	bitmapFile.palette.resize(3);
+	EXPECT_THROW(bitmapFile.VerifyIndexedPaletteSizeDoesNotExceedBitCount(), std::runtime_error);
+}
+
+TEST(BitmapFile, VerifyPixelSizeMatchesImageDimensionsWithPitch)
+{
+	EXPECT_NO_THROW(BitmapFile::VerifyPixelSizeMatchesImageDimensionsWithPitch(1, 1, 1, 4));
+	EXPECT_THROW(BitmapFile::VerifyPixelSizeMatchesImageDimensionsWithPitch(1, 1, 1, 1), std::runtime_error);
+
+	// Test non-static version of function
+	BitmapFile bitmapFile;
+	bitmapFile.imageHeader = ImageHeader::Create(1, 1, 1);
+	bitmapFile.pixels.resize(4, 0);
+	EXPECT_NO_THROW(bitmapFile.VerifyPixelSizeMatchesImageDimensionsWithPitch());
+	bitmapFile.pixels.resize(1, 0);
+	EXPECT_THROW(bitmapFile.VerifyPixelSizeMatchesImageDimensionsWithPitch(), std::runtime_error);
+}
