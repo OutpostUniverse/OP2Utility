d9c8ea4aa309996fdd47fd06239ea3f6fc1e7a29
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-11-11 20:49:50 +0700

    Extend XFile::GetDirectory tests
    Note: These tests are making some opinionated choices. We may wish to
    review what is the "correct" return value.

--- test/XFile.test.cpp
+++ test/XFile.test.cpp
@@ -2,8 +2,38 @@
 #include <gtest/gtest.h>
 
 
-TEST(XFile, GetDirectory) {
-	// Relative path to file
+TEST(XFileGetDirectory, EmptyPath) {
+	EXPECT_EQ("", XFile::GetDirectory(""));
+}
+
+TEST(XFileGetDirectory, RelativePathToFile) {
 	EXPECT_EQ("a/", XFile::GetDirectory("a/file.ext"));
 	EXPECT_EQ("a/b/", XFile::GetDirectory("a/b/file.ext"));
 }
+
+TEST(XFileGetDirectory, RelativePathToDirectory) {
+	EXPECT_EQ("a/", XFile::GetDirectory("a/"));
+	EXPECT_EQ("a/b/", XFile::GetDirectory("a/b/"));
+}
+
+TEST(XFileGetDirectory, PosixAbsolutePathToFile) {
+	EXPECT_EQ("/a/", XFile::GetDirectory("/a/file.ext"));
+	EXPECT_EQ("/a/b/", XFile::GetDirectory("/a/b/file.ext"));
+}
+
+TEST(XFileGetDirectory, PosixAbsolutePathToDirectory) {
+	EXPECT_EQ("/a/", XFile::GetDirectory("/a/"));
+	EXPECT_EQ("/a/b/", XFile::GetDirectory("/a/b/"));
+}
+
+#ifdef _WIN32
+TEST(XFileGetDirectory, WindowsAbsolutePathToFile) {
+	EXPECT_EQ("C:/a/", XFile::GetDirectory("C:/a/file.ext"));
+	EXPECT_EQ("C:/a/b/", XFile::GetDirectory("C:/a/b/file.ext"));
+}
+
+TEST(XFileGetDirectory, WindowsAbsolutePathToDirectory) {
+	EXPECT_EQ("C:/a/", XFile::GetDirectory("C:/a/"));
+	EXPECT_EQ("C:/a/b/", XFile::GetDirectory("C:/a/b/"));
+}
+#endif
