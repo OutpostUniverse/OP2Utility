0cab6ee002633f1146f1112de0131c1054faa6cf
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-12-11 08:42:14 +0700

    Add test for rect width/height calculation
    This test verifies dimensions can be calculated for `const` objects.
    Previously, before methods were marked as const,  these tests would fail
    to compile.

--- test/Rect.test.cpp
+++ test/Rect.test.cpp
@@ -31,3 +31,14 @@ TEST_F(SimpleRect, Height)
 {
 	EXPECT_EQ(y2 - y1, rect.Height());
 }
+
+
+TEST(Rect, CanComputeWidthAndHeight) {
+	const Rect rect1{0, 0, 640, 480};
+	EXPECT_EQ(640, rect1.Width());
+	EXPECT_EQ(480, rect1.Height());
+
+	const Rect rect2{10, 10, 12, 15};
+	EXPECT_EQ(2, rect2.Width());
+	EXPECT_EQ(5, rect2.Height());
+}
