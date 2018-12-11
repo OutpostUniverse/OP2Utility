b7e072b1f54cc16ed697e14e1316297a9a4ba95a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2018-12-11 16:07:02 +0700

    Add Rect comparison operator tests

--- test/Rect.test.cpp
+++ test/Rect.test.cpp
@@ -11,3 +11,18 @@ TEST(Rect, CanComputeWidthAndHeight) {
 	EXPECT_EQ(2, rect2.Width());
 	EXPECT_EQ(5, rect2.Height());
 }
+
+TEST(Rect, ComparisonOperators) {
+	const Rect rect1{0, 0, 640, 480};
+	const Rect rect2{0, 0, 640, 480};
+	const Rect rect3{10, 10, 12, 15};
+
+	// Note: Internally EXPECT_EQ uses `operator ==`
+	EXPECT_EQ(rect1, rect1);
+	EXPECT_EQ(rect1, rect2);
+	EXPECT_EQ(rect2, rect1);
+	EXPECT_EQ(rect2, rect2);
+	// Note: Internally EXPECT_NE uses `operator !=`
+	EXPECT_NE(rect1, rect3);
+	EXPECT_NE(rect2, rect3);
+}
