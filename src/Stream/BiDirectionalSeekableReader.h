d13b90705d0463d70b81ad14d684658df73636e4
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-01-20 21:54:25 -0500

    Create ForwardSeekableReader/Writer classes

--- src/Stream/BiDirectionalSeekableReader.h
+++ src/Stream/BiDirectionalSeekableReader.h
@@ -1,23 +1,14 @@
 #pragma once
 
-#include "Reader.h"
+#include "ForwardSeekableReader.h"
 #include <cstdint>
 
 namespace Stream
 {
-	class BiDirectionalSeekableReader : public Reader {
+	class BiDirectionalSeekableReader : public ForwardSeekableReader {
 	public:
-		// Get the size of the stream
-		virtual uint64_t Length() = 0;
-
-		// Get the current position of the stream
-		virtual uint64_t Position() = 0;
-
 		// Seek to absolute position, given as offset from beginning of stream
 		virtual void Seek(uint64_t position) = 0;
-
-		// Seek forward by a relative amount, given as offset from current position
-		virtual void SeekForward(uint64_t offset) = 0;
 		
 		// Seek backward by a relative amount, given as offset from current position
 		virtual void SeekBackward(uint64_t offset) = 0;
--- src/Stream/BiDirectionalSeekableWriter.h
+++ src/Stream/BiDirectionalSeekableWriter.h
@@ -1,24 +1,15 @@
 #pragma once
 
-#include "Writer.h"
+#include "ForwardSeekableWriter.h"
 #include <cstdint>
 
 namespace Stream
 {
-	class BiDirectionalSeekableWriter : public Writer
+	class BiDirectionalSeekableWriter : public ForwardSeekableWriter
 	{
 	public:
-		// Get the size of the stream
-		virtual uint64_t Length() = 0;
-
-		// Get the current position of the stream
-		virtual uint64_t Position() = 0;
-
 		// Seek to absolute position, given as offset from beginning of stream
 		virtual void Seek(uint64_t offset) = 0;
-
-		// Seek forward by a relative amount, given as offset from current position
-		virtual void SeekForward(uint64_t offset) = 0;
 
 		// Seek backward by a relative amount, given as offset from current position
 		virtual void SeekBackward(uint64_t offset) = 0;
