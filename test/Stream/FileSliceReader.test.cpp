8b11c4cabbb43810db14599769d4a684883d4787
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-02-02 18:32:04 +0700

    Add FileSliceReader bounds check tests

--- test/Stream/FileSliceReader.test.cpp
+++ test/Stream/FileSliceReader.test.cpp
@@ -8,3 +8,16 @@ Stream::FileSliceReader CreateSeekableReader<Stream::FileSliceReader>() {
 }
 
 INSTANTIATE_TYPED_TEST_CASE_P(FileSliceReader, SimpleSeekableReader, Stream::FileSliceReader);
+
+
+TEST(FileSliceReader, SliceBoundsAreChecked) {
+	Stream::FileReader stream("Stream/data/SimpleStream.txt");
+
+	// Proects against overflow of the stream position
+	EXPECT_THROW(stream.Slice(1, std::numeric_limits<uint64_t>::max()), std::runtime_error);
+	EXPECT_THROW(stream.Slice(std::numeric_limits<uint64_t>::max(), 1), std::runtime_error);
+
+	// Protects against overstepping the bounds of the source stream
+	EXPECT_THROW(stream.Slice(0, stream.Length() + 1), std::runtime_error);
+	EXPECT_THROW(stream.Slice(stream.Length(), 1), std::runtime_error);
+}
