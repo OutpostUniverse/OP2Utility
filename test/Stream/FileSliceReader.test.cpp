7d2f649e4b34f841ce8e918bdb3871f6f0a5a579
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-02-03 14:23:16 +0700

    Add test if slice of slice is at correct offset
    Slice parameter values should be in the domain of the slice. Currently
    some of them look to be in the domain of the wrapped stream. This makes
    the checks and usage of those values inconsistent. This test should help
    catch such errors.

--- test/Stream/FileSliceReader.test.cpp
+++ test/Stream/FileSliceReader.test.cpp
@@ -49,3 +49,20 @@ TEST(FileSliceReader, SliceOfSliceIsBoundsChecked) {
 	EXPECT_THROW(slice.Slice(1, slice.Length()), std::runtime_error);
 	EXPECT_THROW(slice.Slice(slice.Length(), 1), std::runtime_error);
 }
+
+TEST(FileSliceReader, SliceOfSliceMatchesCorrectOffset) {
+	Stream::FileReader stream("Stream/data/SimpleStream.txt");
+	auto slice1 = stream.Slice(1, stream.Length() - 2);
+	auto slice2 = slice1.Slice(1, slice1.Length() - 2);
+
+	stream.Seek(2);
+	slice1.Seek(1);
+
+	char data, data1, data2;
+	stream.Read(data);
+	slice1.Read(data1);
+	slice2.Read(data2);
+
+	EXPECT_EQ(data, data1);
+	EXPECT_EQ(data, data2);
+}
