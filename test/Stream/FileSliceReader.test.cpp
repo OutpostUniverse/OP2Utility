5722cdbe5130d2de0b0c6af690637a59f936dd89
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-02-03 13:50:59 +0700

    Add tests for slicing of a slice

--- test/Stream/FileSliceReader.test.cpp
+++ test/Stream/FileSliceReader.test.cpp
@@ -10,7 +10,7 @@ Stream::FileSliceReader CreateSeekableReader<Stream::FileSliceReader>() {
 INSTANTIATE_TYPED_TEST_CASE_P(FileSliceReader, SimpleSeekableReader, Stream::FileSliceReader);
 
 
-TEST(FileSliceReader, SliceBoundsAreChecked) {
+TEST(FileSliceReader, SliceIsBoundsChecked) {
 	Stream::FileReader stream("Stream/data/SimpleStream.txt");
 
 	// Proects against overflow of the stream position
@@ -19,5 +19,33 @@ TEST(FileSliceReader, SliceBoundsAreChecked) {
 
 	// Protects against overstepping the bounds of the source stream
 	EXPECT_THROW(stream.Slice(0, stream.Length() + 1), std::runtime_error);
+	EXPECT_THROW(stream.Slice(1, stream.Length()), std::runtime_error);
 	EXPECT_THROW(stream.Slice(stream.Length(), 1), std::runtime_error);
+}
+
+TEST(FileSliceReader, SliceCanBeSliced) {
+	Stream::FileReader stream("Stream/data/SimpleStream.txt");
+	auto slice = stream.Slice(1, stream.Length() - 2);
+
+	// Creating a 0 byte (empty) slice works
+	EXPECT_NO_THROW(slice.Slice(0, 0));
+	EXPECT_NO_THROW(slice.Slice(slice.Length(), 0));
+
+	// Creating a full sized slice works
+	EXPECT_NO_THROW(slice.Slice(0, slice.Length()));
+
+	// Slicing in works
+	EXPECT_NO_THROW(slice.Slice(0, slice.Length() - 1));  // Chop end
+	EXPECT_NO_THROW(slice.Slice(1, slice.Length() - 1));  // Chop beginning
+	EXPECT_NO_THROW(slice.Slice(1, slice.Length() - 2));  // Chop both sides
+}
+
+TEST(FileSliceReader, SliceOfSliceIsBoundsChecked) {
+	Stream::FileReader stream("Stream/data/SimpleStream.txt");
+	auto slice = stream.Slice(1, stream.Length() - 2);
+
+	// Protects against overstepping the bounds of the original slice
+	EXPECT_THROW(slice.Slice(0, slice.Length() + 1), std::runtime_error);
+	EXPECT_THROW(slice.Slice(1, slice.Length()), std::runtime_error);
+	EXPECT_THROW(slice.Slice(slice.Length(), 1), std::runtime_error);
 }
