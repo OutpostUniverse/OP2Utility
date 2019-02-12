503a46145ccd12bfa050866e7cff1b3189d80e47
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-02-12 21:00:53 +0700

    Add DynamicMemoryWriter class
    This writes to a dynamically allocated and resizable memory buffer.
    After writing, the `GetReader()` method will return a `MemoryReader`
    object with a view of the memory.
    
    This class can be used to aid writing test code. Writing can be done
    in-memory, and then verified with the associated `MemoryReader` object.
    There is no need to write to a slow disk, nor any need to clean up
    temporary files. This makes writing test code simpler and faster.

---
+++ src/Stream/DynamicMemoryWriter.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "BiDirectionalSeekableWriter.h"
+#include "MemoryReader.h"
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+namespace Stream
+{
+	class DynamicMemoryWriter : public BidirectionalSeekableWriter
+	{
+	public:
+		DynamicMemoryWriter();
+		DynamicMemoryWriter(std::size_t preallocateSize);
+
+		uint64_t Length() override;
+		uint64_t Position() override;
+
+		// Note: SeekForward will set a new stream size and 0 fill the buffer gap
+		void SeekForward(uint64_t offset) override;
+		// Note: SeekBackward will set a new stream size and truncate the stream
+		void SeekBackward(uint64_t offset) override;
+		// Note: Seek will set a new stream size (either by 0 filling or by truncating)
+		void Seek(uint64_t position) override;
+
+		// Get read access to the written data
+		MemoryReader GetReader();
+
+	protected:
+		void WriteImplementation(const void* buffer, std::size_t size) override;
+
+	private:
+		std::vector<uint8_t> streamBuffer;
+	};
+}
