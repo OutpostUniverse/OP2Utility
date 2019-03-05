e5f3feb433a941622c00beca0c7d40a2863a47be
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-03-05 18:40:34 +0700

    Update documentation for DynamicMemoryReader

--- src/Stream/DynamicMemoryWriter.h
+++ src/Stream/DynamicMemoryWriter.h
@@ -11,10 +11,16 @@ namespace Stream
 	/**
 	 * \brief Memory backed stream, which grows dynamically in size as data is added
 	 *
+	 * This stream can be used as a destination when the output size is not known exactly ahead of time.
+	 * Written data can later be accessed by calling GetReader to retrieve a MemoryReader for the written data.
+	 * The DynamicMemoryWriter is particularly useful when writing test code for stream serializaation,
+	 * as it does not need to write to disk, nor cleanup temporary files, during round-trip testing.
+	 *
 	 * An internal memory buffer is managed by DynamicMemoryWriter, which grows as needed to accommodate new data.
 	 * This is in contrast to MemoryWriter, which is a view to a non-owned pre-set sized buffer, which can not grow.
 	 *
-	 * \note Adding additional data to the stream after calling GetReader may invalidate existing readers.
+	 * \note Adding additional data to the stream after calling GetReader invalidates existing readers.
+	 * (They may be left pointing at old memory after a re-allocation).
 	 */
 	class DynamicMemoryWriter : public BidirectionalWriter
 	{
