55ddfa08d09f33220fb464d26542bffc2fc57bd4
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-04-17 20:50:03 -0400

    Add test to ensure Resource Manager can be created on a directory that contains sub-directories XXX.vol and XXX.clm
    Use .keep files to allow git to track empty directories.

--- test/ResourceManager.test.cpp
+++ test/ResourceManager.test.cpp
@@ -12,3 +12,8 @@ TEST(ResourceManager, RefusesAbsolutePaths) {
 	EXPECT_THROW(resourceManager.GetResourceStream("/Archive.vol"), std::runtime_error);
 	EXPECT_THROW(resourceManager.GetResourceStream("/PathTo/Archive.vol"), std::runtime_error);
 }
+
+TEST(ResourceManager, IgnoreNonFilenamesOnConstruction) {
+	// Attempt to load Resource Manager where directories exist with .vol and .clm 'extensions'
+	EXPECT_NO_THROW(ResourceManager resourceManager("./data"));
+}
