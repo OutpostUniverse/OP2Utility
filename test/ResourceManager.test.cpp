47fc1a0a6d134bbc8c036059c81f2d4d16e37437
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-04-18 21:17:14 -0400

    Separate ResourceManager units tests
    Was testing GetResourceStream and class construction in the same function. Better to separate out.

--- test/ResourceManager.test.cpp
+++ test/ResourceManager.test.cpp
@@ -2,21 +2,22 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
 
+TEST(ResourceManager, ConstructResourceManager)
+{
+	// Load Resource Manager where directories exist with .vol and .clm 'extensions'
+	EXPECT_NO_THROW(ResourceManager("./data"));
 
-TEST(ResourceManager, ConstructResourceManager) {
+	// Refuse to load when passed a filename
+	EXPECT_THROW(ResourceManager("./data/Directory.clm/.keep"), std::runtime_error);
+}
+
+TEST(ResourceManager, GetResourceStream_RefuseAbsolutePath) {
 	ResourceManager resourceManager("");
 
-	// Refuse absolute paths
 #ifdef _WIN32
 	EXPECT_THROW(resourceManager.GetResourceStream("C:/Archive.vol"), std::runtime_error);
 	EXPECT_THROW(resourceManager.GetResourceStream("C:/PathTo/Archive.vol"), std::runtime_error);
 #endif
 	EXPECT_THROW(resourceManager.GetResourceStream("/Archive.vol"), std::runtime_error);
 	EXPECT_THROW(resourceManager.GetResourceStream("/PathTo/Archive.vol"), std::runtime_error);
-
-	// Load Resource Manager where directories exist with .vol and .clm 'extensions'
-	EXPECT_NO_THROW(ResourceManager("./data"));
-
-	// Refuse to load when passed a filename
-	EXPECT_THROW(ResourceManager("./data/Directory.clm/.keep"), std::runtime_error);
 }
