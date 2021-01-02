9510b0bc53c5f9910d3a2f628f8992bd5d78c9de
Brett208 <15183337+Brett208@users.noreply.github.com>
2021-01-01 19:08:54 -0500

    Extract error message formatting from tileset validation function
    Use named constants for default values in validation

--- src/Sprite/TilesetLoader.cpp
+++ src/Sprite/TilesetLoader.cpp
@@ -1,10 +1,16 @@
 #include "TilesetLoader.h"
 #include "../Bitmap/BitmapFile.h"
 #include "../Stream/BidirectionalReader.h"
+#include <cstdint>
 #include <stdexcept>
 
 namespace Tileset
 {
+	void throwReadError(std::string propertyName, std::string value, std::string expectedValue);
+	void throwReadError(std::string propertyName, uint32_t value, uint32_t expectedValue);
+
+	std::string formatReadErrorMessage(std::string propertyName, std::string value, std::string expectedValue);
+
 	bool PeekIsCustomTileset(Stream::BidirectionalReader& reader)
 	{
 		Tag tag;
@@ -16,19 +22,36 @@ namespace Tileset
 
 	void ValidateTileset(const BitmapFile& tileset)
 	{
-		if (tileset.imageHeader.bitCount != 8)
-		{
-			throw std::runtime_error("Tileset palette must be an 8 bit indexed bitmap");
+		constexpr uint32_t DefaultPixelWidth = 32;
+		constexpr uint32_t DefaultPixelHeightMultiple = DefaultPixelWidth;
+		constexpr uint32_t DefaultBitDepth = 8;
+
+		if (tileset.imageHeader.bitCount != DefaultBitDepth) {
+			throwReadError("Bit Depth", tileset.imageHeader.bitCount, DefaultBitDepth);
 		}
 
-		if (tileset.imageHeader.width != 32)
-		{
-			throw std::runtime_error("Tileset width must be exactly 32 pixels");
+		if (tileset.imageHeader.width != DefaultPixelWidth) {
+			throwReadError("Pixel Width", tileset.imageHeader.width, DefaultPixelWidth);
 		}
 
-		if (tileset.imageHeader.height % 32 != 0)
-		{
-			throw std::runtime_error("Tileset height must be a multiple of 32 pixels");
+		if (tileset.imageHeader.height % DefaultPixelHeightMultiple != 0) {
+			throw std::runtime_error("Tileset property Pixel Height reads " + std::to_string(tileset.imageHeader.height) +
+				". Expected value must be a multiple of " + std::to_string(DefaultPixelHeightMultiple) + " pixels");
 		}
+	}
+
+	void throwReadError(std::string propertyName, std::string value, std::string expectedValue)
+	{
+		throw std::runtime_error(formatReadErrorMessage(propertyName, value, expectedValue));
+	}
+
+	void throwReadError(std::string propertyName, uint32_t value, uint32_t expectedValue)
+	{
+		throwReadError(propertyName, std::to_string(value), std::to_string(expectedValue));
+	}
+
+	std::string formatReadErrorMessage(std::string propertyName, std::string value, std::string expectedValue)
+	{
+		return "Tileset property " + propertyName + " reads. Expected a value of " + expectedValue + ".";
 	}
 }
