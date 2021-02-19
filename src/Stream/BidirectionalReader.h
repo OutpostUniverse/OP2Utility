208676e67853a4452f081633fa6d68c1854f570e
Brett208 <15183337+Brett208@users.noreply.github.com>
2021-02-18 21:39:01 -0500

    Remove PeakImplementation virtual function

--- src/Stream/BidirectionalReader.h
+++ src/Stream/BidirectionalReader.h
@@ -6,26 +6,17 @@
 namespace OP2Utility::Stream
 {
 	class BidirectionalReader : public ForwardReader {
-	protected:
-		// Generic peek method, which raises an exception if the full data cannot be read
-		// Note: This is named separately from Peek to prevent name hiding in derived classes
-		// All Peek methods are syntax sugar which delegate to this method
-		void PeekImplementation(void* buffer, std::size_t size)
-		{
-			ReadImplementation(buffer, size);
-			SeekBackward(size);
-		}
-
 	public:
 		void Peek(void* buffer, std::size_t size) {
-			PeekImplementation(buffer, size);
+			ReadImplementation(buffer, size);
+			SeekBackward(size);
 		}
 
 		// Trivially copyable data types
 		template<typename T>
 		std::enable_if_t<std::is_trivially_copyable<T>::value>
 			Peek(T& object) {
-			PeekImplementation(&object, sizeof(object));
+			Peek(&object, sizeof(object));
 		}
 
 		// Seek backward by a relative amount, given as offset from current position
