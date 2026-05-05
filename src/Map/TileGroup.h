8458d2ff32e0d1452ab785554326f099e7dab312
Daniel Stevens <Dan.R.Stevens@gmail.com>
2026-05-05 00:39:13 -0600

    Ensure fields in `TileGroup` are initialized
    Fixes MSVC Code Analysis warnings.
    > warning C26495: Variable 'X' is uninitialized. Always initialize a member variable (type.6).

--- src/Map/TileGroup.h
+++ src/Map/TileGroup.h
@@ -12,10 +12,10 @@ namespace OP2Utility
 		std::string name;
 
 		// Width of tile group in tiles
-		uint32_t tileWidth;
+		uint32_t tileWidth{};
 
 		// Height of tile group in tiles
-		uint32_t tileHeight;
+		uint32_t tileHeight{};
 
 		// Tiles used in TileGroup listed in 1D (all of row 0 tiles first, then row 1, etc).
 		std::vector<uint32_t> mappingIndices;
