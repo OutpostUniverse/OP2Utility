59bbeb324aae3293ebdb18b9906d0af04937e140
Daniel Stevens <Dan.R.Stevens@gmail.com>
2026-05-06 04:52:40 -0600

    Change element type in DeBruijn multiplication table
    Fix Clang warning `-Wsign-conversion`.

--- src/BitTwiddle.cpp
+++ src/BitTwiddle.cpp
@@ -10,7 +10,7 @@ namespace OP2Utility
 	// This method assumes the argument is a power of 2
 	uint32_t Log2OfPowerOf2(uint32_t value)
 	{
-		static const int MultiplyDeBruijnBitPosition2[32] =
+		static const uint32_t MultiplyDeBruijnBitPosition2[32] =
 		{
 			0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
 			31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
